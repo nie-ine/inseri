e0752b5c40a83d79354d6c9783aee18efcd8544b
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-01-10 14:10:20 +0100

    Add some more basic content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+		BrowserModule,
+		CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
