94ccd405d583137ba8327f430fc3d6bd4216b34a
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-01-19 11:01:04 +0100

    Add basic view components for EditionViewModule

---
+++ src/app/edition-view/edition-view-tools/edition-view-tools.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edition-view-tools',
+  templateUrl: './edition-view-tools.component.html',
+  styleUrls: ['./edition-view-tools.component.scss']
+})
+export class EditionViewToolsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/edition-view/edition-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edition-view',
+  templateUrl: './edition-view.component.html',
+  styleUrls: ['./edition-view.component.scss']
+})
+export class EditionViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
