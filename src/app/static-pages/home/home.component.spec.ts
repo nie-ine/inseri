873d8929996bcd74498d51e71a4eebbfee2f6127
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-01-30 12:14:31 +0100

    added used component to spec.ts

--- src/app/static-pages/home/home.component.spec.ts
+++ src/app/static-pages/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { ExampleComponent } from 'nie-ine';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent, ExampleComponent ]
     })
     .compileComponents();
   }));
