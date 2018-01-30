48876dd7ea3367964f47e74e37db149e73a1f346
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-01-30 11:06:00 +0100

    added working nie-ine-npm component

--- src/app/static-pages/static-pages.module.ts
+++ src/app/static-pages/static-pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ExampleComponent } from 'nie-ine';
 
 @NgModule({
   imports: [
@@ -10,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 		RouterModule.forChild([
 			{ path: 'about', component: AboutComponent },
 			{ path: 'home', component: HomeComponent },
+      { path: 'example', component: ExampleComponent },
 			{ path: '', redirectTo: '/home', pathMatch: 'full'}
 		])
   ],
