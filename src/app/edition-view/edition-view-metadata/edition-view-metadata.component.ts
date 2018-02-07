5a51dd9f9f9897b74e3e011cede8f66d9a9d56a5
Unknown <robertalaura.padlina@unibas.ch>
2018-02-07 09:39:12 +0100

    Adapted metadata for edition text (title belongs to metadata, not to the edited text).

--- src/app/edition-view/edition-view-metadata/edition-view-metadata.component.ts
+++ src/app/edition-view/edition-view-metadata/edition-view-metadata.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Person} from '../../shared/models/person';
+import {JulianDate} from '../../shared/models/julian-date';
 
 @Component({
   selector: 'app-edition-view-metadata',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edition-view-metadata.component.scss']
 })
 export class EditionViewMetadataComponent implements OnInit {
+
+  author: Person;
+  title: string;
+  date: JulianDate;
+  place: string;
+  editor: Person;
 
   constructor() { }
 
