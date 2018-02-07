ca7693df5373ac44079142489cb7141f46fabda9
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2018-02-07 16:40:14 +0100

    insert image-frame from package

--- src/app/image-view/image-view.module.ts
+++ src/app/image-view/image-view.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule} from '@angular/router';
 import { ImageFrameModule } from './image-frame/image-frame.module';
 import { ImageViewComponent } from './image-view.component';
+import { ArithmeticModule } from 'nie-ine';
 
 @NgModule({
   imports: [
     CommonModule,
+    ArithmeticModule,
     ImageFrameModule,
     RouterModule.forChild([
       { path: 'image-view', component: ImageViewComponent }
