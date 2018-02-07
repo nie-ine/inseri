bd933841a311e9ac4e591a64a7c7712cf00fe9b7
Unknown <robertalaura.padlina@unibas.ch>
2018-02-07 09:39:48 +0100

    Improved edition view module.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -9,17 +9,24 @@ import {FooterComponent} from './footer/footer.component';
 import {MainComponent} from './main/main.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {NavigationComponent} from './navigation/navigation.component';
+import {EditionViewModule} from '../edition-view/edition-view.module';
+import {KnoraRequestService} from '../shared/knora-request.service';
+import {SparqlRequestService} from '../shared/sparql-request.service';
+import {ResultToEditionMapperService} from '../edition-view/result-to-edition-mapper.service';
+import {KnoraAuthService} from '../shared/knora-auth.service';
 
 @NgModule({
   imports: [
     CommonModule,
     StaticPagesModule,
+    EditionViewModule,
     MaterialModule,
     RouterModule.forRoot([
       {path: '**', component: PageNotFoundComponent}
     ])
   ],
   declarations: [HeaderComponent, FooterComponent, MainComponent, PageNotFoundComponent, NavigationComponent],
+  providers: [KnoraRequestService, SparqlRequestService, ResultToEditionMapperService, KnoraAuthService],
   exports: [HeaderComponent, FooterComponent, MainComponent, RouterModule]
 })
 export class CoreModule {
--- src/app/edition-view/edition-view.component.ts
+++ src/app/edition-view/edition-view.component.ts
@@ -10,6 +10,8 @@ import {ResultToEditionMapperService} from './result-to-edition-mapper.service';
 })
 export class EditionViewComponent implements OnInit {
 
+  dummyText = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+
   constructor(knoraRequestService: KnoraRequestService,
               sparqlRequestService: SparqlRequestService,
               resultToEditionMapperService: ResultToEditionMapperService) {
