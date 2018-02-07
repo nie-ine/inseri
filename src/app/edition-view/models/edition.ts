da827aa06daa75b2ac591048b9b67395e281d6a0
Unknown <robertalaura.padlina@unibas.ch>
2018-02-07 09:39:12 +0100

    Adapted metadata for edition text (title belongs to metadata, not to the edited text).

--- src/app/edition-view/models/edition.ts
+++ src/app/edition-view/models/edition.ts
@@ -1,13 +1,12 @@
 import {Person} from '../../shared/models/person';
 import {JulianDate} from '../../shared/models/julian-date';
 import {Place} from '../../shared/models/place';
-import {EditedText} from './edited-text';
 import {BasicModel} from '../../shared/models/basic-model';
 
 export class Edition extends BasicModel {
   editor: Person[];
   title?: string;
-  text: EditedText;
+  text: string;
   date?: JulianDate;
   place?: Place;
 }
