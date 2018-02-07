1db12bebe5b8d02a518ec5faf191241e2bba5c67
Unknown <robertalaura.padlina@unibas.ch>
2018-02-07 09:36:32 +0100

    Files refactored.

--- src/app/edition-view/edition-view.module.ts
+++ src/app/edition-view/edition-view.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { EditionViewComponent } from './edition-view.component';
-import { EditionViewToolsComponent } from './edition-view-tools/edition-view-tools.component';
-import { EditionViewStructureComponent } from './edition-view-structure/edition-view-structure.component';
-import { EditionViewMetadataComponent } from './edition-view-metadata/edition-view-metadata.component';
-import { EditionViewCanvasComponent } from './edition-view-canvas/edition-view-canvas.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EditionViewComponent} from './edition-view.component';
+import {EditionViewToolsComponent} from './edition-view-tools/edition-view-tools.component';
+import {EditionViewStructureComponent} from './edition-view-structure/edition-view-structure.component';
+import {EditionViewMetadataComponent} from './edition-view-metadata/edition-view-metadata.component';
+import {EditionViewCanvasComponent} from './edition-view-canvas/edition-view-canvas.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
-  ],
-  declarations: [EditionViewComponent, EditionViewToolsComponent, EditionViewStructureComponent, EditionViewMetadataComponent, EditionViewCanvasComponent]
+    CommonModule,
+    RouterModule.forChild([
+      {path: 'edition-view', component: EditionViewComponent}
+    ])],
+  declarations: [
+    EditionViewComponent,
+    EditionViewToolsComponent,
+    EditionViewStructureComponent,
+    EditionViewMetadataComponent,
+    EditionViewCanvasComponent],
 })
 export class EditionViewModule {
 
--- src/app/static-pages/static-pages.module.ts
+++ src/app/static-pages/static-pages.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './home/home.component';
+import {MaterialModule} from '../material.module';
 
 @NgModule({
   imports: [
-		CommonModule,
-		RouterModule.forChild([
-			{ path: 'about', component: AboutComponent },
-			{ path: 'home', component: HomeComponent },
-			{ path: '', redirectTo: '/home', pathMatch: 'full'}
-		])
+    CommonModule,
+    RouterModule.forChild([
+      {path: 'about', component: AboutComponent},
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full'}
+    ]),
+    MaterialModule,
   ],
-	declarations: [AboutComponent, HomeComponent],
-	exports: [AboutComponent, HomeComponent]
+  declarations: [AboutComponent, HomeComponent],
+  exports: [AboutComponent, HomeComponent]
 })
-export class StaticPagesModule { }
+export class StaticPagesModule {
+}
