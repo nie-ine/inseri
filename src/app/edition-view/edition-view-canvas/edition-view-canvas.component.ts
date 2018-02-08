0ef2dfb70343cf405306e1b17f9546850bb51d55
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-02-08 16:21:20 +0100

    Implement first step of moving focus on highlight

--- src/app/edition-view/edition-view-canvas/edition-view-canvas.component.ts
+++ src/app/edition-view/edition-view-canvas/edition-view-canvas.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import {Component, ElementRef, Input, OnChanges, ViewChild} from '@angular/core';
 import {StandoffReconcilerService} from '../../shared/standoff-reconciler.service';
 import {Standoff} from '../../shared/models/standoff';
 import {CanvasOptionsService} from '../canvas-options.service';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-edition-view-canvas',
@@ -13,13 +14,23 @@ export class EditionViewCanvasComponent implements OnChanges {
 
   @Input() text: string;
   @Input() standoffs: Standoff[];
+
   standoffHtml: string;
-  html: string;
+  html: SafeHtml;
   nightView: boolean;
   fontSize = 100;
   private findTerm: string;
+  numberOfMatches = 0;
+  focusedMatchIndex = 1;
 
-  constructor(private standoffReconcilerService: StandoffReconcilerService, private canvasOptionsService: CanvasOptionsService) {
+  private static replaceAndCountOccurrences(text: string, regex: RegExp, selectIndex?: number): [string, number] {
+    let counter = 0;
+    return [text.replace(regex,
+      (x) => '<span id="occ' + ++counter + '" class="highlight ' + (counter === selectIndex ? 'focused' : '') + '">' + x + '</span>'
+    ), counter];
+  }
+
+  constructor(private standoffReconcilerService: StandoffReconcilerService, private canvasOptionsService: CanvasOptionsService, private domSanitizer: DomSanitizer) {
     canvasOptionsService.nightView$.subscribe(state => this.nightView = state);
     canvasOptionsService.fontSize$.subscribe(size => this.fontSize = size);
     canvasOptionsService.term$.subscribe(term => {
@@ -32,13 +43,31 @@ export class EditionViewCanvasComponent implements OnChanges {
     this.updateText();
   }
 
+  decreaseIndexOfFocus() {
+    this.focusedMatchIndex = ((this.focusedMatchIndex - 1) <= 0) ? this.numberOfMatches : this.focusedMatchIndex - 1;
+    this.highlight();
+  }
+
+  increaseIndexOfFocus() {
+    this.focusedMatchIndex = ((this.focusedMatchIndex + 1) > this.numberOfMatches) ? 1 : this.focusedMatchIndex + 1;
+    this.highlight();
+  }
+
   private highlight() {
-    const regex = new RegExp('(' + this.findTerm + ')', 'gi');
-    this.html = (!this.findTerm || this.findTerm === '') ? this.standoffHtml : this.standoffHtml.replace(regex, '<span class="highlight">$1</span>');
+    if (!this.findTerm || this.findTerm === '') {
+      this.html = this.standoffHtml;
+      this.numberOfMatches = 0;
+    } else {
+      const regex = new RegExp('(' + this.findTerm + ')', 'gi');
+      const occurrences = EditionViewCanvasComponent.replaceAndCountOccurrences(this.standoffHtml, regex, this.focusedMatchIndex);
+      this.html = this.domSanitizer.bypassSecurityTrustHtml(occurrences[0]);
+      this.numberOfMatches = occurrences[1];
+    }
   }
 
   private updateText() {
     this.standoffHtml = this.standoffReconcilerService.reconcile(this.text, this.standoffs);
     this.highlight();
   }
+
 }
