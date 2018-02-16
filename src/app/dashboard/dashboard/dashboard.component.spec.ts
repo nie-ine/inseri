c32fbbb9260e6339ddb56ac71fef1a2760a5074a
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-02-16 10:52:00 +0100

    added test

--- src/app/dashboard/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+
 import { DashboardComponent } from './dashboard.component';
+import { MatDialog } from '@angular/material/dialog';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,7 +10,11 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [
+        // workaround: why I can't inject MatDialogRef in the unit test?
+        {provide: MatDialog, useValue: {}}
+  ]
     })
     .compileComponents();
   }));
