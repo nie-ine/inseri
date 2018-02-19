8ca72fc34841aa4c8c9ee83e9f92e86a01ea8016
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2018-02-19 15:21:13 +0100

    add link to linked resource

--- src/app/metadata-view/fact-sheet/fact-sheet.module.ts
+++ src/app/metadata-view/fact-sheet/fact-sheet.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FactSheetComponent } from './fact-sheet/fact-sheet.component';
 import { IndirectEntryComponent } from './indirect-entry/indirect-entry.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [FactSheetComponent, IndirectEntryComponent],
   exports: [FactSheetComponent]
