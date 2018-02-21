21c9e55a7bacca2890a39889be1727ad8b4a2808
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2018-02-21 17:29:38 +0100

    add ImageWithOverlayComponent that accepts also regions

--- src/app/image-view/image-frame/image-frame.module.ts
+++ src/app/image-view/image-frame/image-frame.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { ImageFrameComponent } from './image-frame/image-frame.component';
 import { ImageFrameSizesComponent } from './image-frame-sizes/image-frame-sizes.component';
-import { FormsModule } from '@angular/forms';
+import { ImageWithOverlayComponent } from './image-with-overlay/image-with-overlay.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [ImageFrameComponent, ImageFrameSizesComponent],
-  exports: [ImageFrameComponent, ImageFrameSizesComponent]
+  declarations: [ImageFrameComponent, ImageFrameSizesComponent, ImageWithOverlayComponent],
+  exports: [ImageFrameComponent, ImageFrameSizesComponent, ImageWithOverlayComponent]
 })
 export class ImageFrameModule { }
