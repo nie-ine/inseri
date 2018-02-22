08587933e9423dc7dae2d81cb00aaee938c9b280
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-02-22 16:00:44 +0100

    almost working appended component with input

--- src/app/grapesjs/grapesjs/grapesjs.component.ts
+++ src/app/grapesjs/grapesjs/grapesjs.component.ts
@@ -1,46 +1,65 @@
-import { Component, OnInit, ComponentRef } from '@angular/core';
+import { Component, OnInit, ComponentRef, AfterContentInit, ElementRef } from '@angular/core';
 import 'rxjs/add/operator/map';
 import {
   Inject,
   ViewContainerRef
 } from '@angular/core';
 import { Service } from '../createComponentInstances.service';
-import {ExampleComponent} from 'nie-ine';
+import {
+  ExampleComponent,
+  ImageFrameComponent
+} from 'nie-ine';
 
 declare var grapesjs: any; // Important!
 
 @Component({
   selector: 'app-grapesjs',
-  template: '<div id="gjs"></div>',
+  templateUrl: './grapesjs.component.html',
   providers: [Service],
   styleUrls: ['./grapesjs.component.scss']
 })
-export class GrapesjsComponent implements OnInit {
+export class GrapesjsComponent {
 
-  name = 'from Angular';
-  testblock: string;
+  imageFrameComponent: any;
+  exampleComponent: any;
+  node: string;
 
-  constructor(private service: Service, private viewContainerRef: ViewContainerRef) {
-    service.setRootViewContainerRef(viewContainerRef);
-    this.testblock = service.addDynamicComponent(new ExampleComponent);
+  picdata = {
+    '@id' : 'http://rdfh.ch/kuno-raeber/Uzo2YDhzTr-8CUSg1pQL4Q/values/gJVf-AQjSbSTAo8EsU8ErQ',
+    '@type' : 'knora-api:StillImageFileValue',
+    'knora-api:fileValueAsUrl' :
+    'https://tools.wmflabs.org/zoomviewer/' +
+    'proxy.php?iiif=Lions_Family_Portrait_Masai_Mara.jpg/pct:65,81,35,15/full/0/default.jpg',
+    'knora-api:fileValueHasFilename' : 'proxy.php?iiif=Lions_Family_Portrait_Masai_Mara.jpg',
+    'knora-api:fileValueIsPreview' : false,
+    'knora-api:stillImageFileValueHasDimX' : 5184,
+    'knora-api:stillImageFileValueHasDimY' : 3456,
+    'knora-api:stillImageFileValueHasIIIFBaseUrl' : 'https://tools.wmflabs.org/zoomviewer'
+  };
+
+
+  constructor(public service: Service, private viewContainerRef: ViewContainerRef, private elementRef: ElementRef) {
+    this.viewContainerRef = viewContainerRef;
+    this.service.setRootViewContainerRef(this.viewContainerRef);
+  }
+  addExampleComponent () {
+    this.exampleComponent = new ExampleComponent;
+    this.service.addDynamicComponent(this.exampleComponent);
+    this.service.addDynamicComponent(new BreakComponent);
   }
 
-  ngOnInit() {
-
-    const editor = grapesjs.init({
-      container : '#gjs',
-      components: '<div class="txt-red">Beginning Template</div>',
-      style: '.txt-red{color: red}',
-    });
-
-    const blockManager = editor.BlockManager;
-
-    blockManager.add('nie-test', {
-      label: 'nie test',
-      attributes: { class:'fa fa-newspaper-o' },
-      content:  this.testblock,
-    });
-
+  addImageComponent () {
+    this.service.addDynamicComponent(new ImageFrameComponent(this.elementRef));
   }
+
 
 }
+
+@Component({
+  selector: 'break-component',
+  template: '<br>',
+})
+export class BreakComponent {
+
+}
+
