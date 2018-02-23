55fdcd6270f22469573f11d3ceefa4e1f155442d
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-02-23 17:00:20 +0100

    Dummy elements are positioned in light table where dropped

--- src/app/synopsis/synopsis-object-data.ts
+++ src/app/synopsis/synopsis-object-data.ts
@@ -3,6 +3,8 @@ export interface SynopsisObjectData {
   name: string;
   height?: string;
   width?: string;
+  top?: number;
+  left?: number;
 }
 
 export class SynopsisTextData implements SynopsisObjectData {
@@ -11,12 +13,16 @@ export class SynopsisTextData implements SynopsisObjectData {
   name: string;
   height: string | undefined;
   width: string | undefined;
+  top: number | undefined;
+  left: number | undefined;
 
-  constructor(name: string, htmlText: string, height?: string, width?: string) {
+  constructor(name: string, htmlText: string, height?: string, width?: string, top?: number, left?: number) {
     this.name = name;
     this.htmlText = htmlText;
     this.height = height;
     this.width = width;
+    this.top = top;
+    this.left = left;
   }
 }
 
@@ -26,11 +32,15 @@ export class SynopsisImageData implements SynopsisObjectData {
   name: string;
   height: string | undefined;
   width: string | undefined;
+  top: number | undefined;
+  left: number | undefined;
 
-  constructor(name: string, src: string, height?: string, width?: string) {
+  constructor(name: string, src: string, height?: string, width?: string, top?: number, left?: number) {
     this.name = name;
     this.src = src;
     this.height = height;
     this.width = width;
+    this.top = top;
+    this.left = left;
   }
 }
