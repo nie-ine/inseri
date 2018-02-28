7afc5195621b02b840e53570afc37cff0528d0ce
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2018-02-28 15:18:03 +0100

    introduce mock request for indirectly linked data

--- src/app/metadata-view/fact-sheet/indirect-entry/indirect-entry.component.ts
+++ src/app/metadata-view/fact-sheet/indirect-entry/indirect-entry.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-indirect-entry',
@@ -8,17 +9,22 @@ import { Component, Input, OnInit } from '@angular/core';
 export class IndirectEntryComponent implements OnInit {
 
   @Input() iri: string;
+  @Input() prop: string;
   uri: string;
-  title = ' ';
+  title: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
     this.uri = encodeURIComponent(this.iri);
 
-    // TODO: use knora service to get the title of the linked resource when the services are ready.
-    this.title = '(include requested title here)';
-
+    // TODO: replace this request with a request through a service
+    // this.httpClient.get('http://130.60.24.65:3338/v2/resources/' + encodeURI(this.iri)).subscribe(res => {
+    this.httpClient.get('http://130.60.24.65:3338/v2/resources/http%3A%2F%2Frdfh.ch%2Fkuno-raeber%2F2Ghtz54mTFiGeNPLjZMTbg')
+      .subscribe(res => {
+      this.title = res['schema:itemListElement']['text:hasConvoluteTitle']['knora-api:valueAsString'];
+      console.log(res);
+    });
   }
 
 }
