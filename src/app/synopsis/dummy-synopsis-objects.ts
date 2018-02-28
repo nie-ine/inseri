908b89af4d4e9e0576acbe130cd6bb295a1f1299
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-02-28 13:23:16 +0100

    Add more example components

--- src/app/synopsis/dummy-synopsis-objects.ts
+++ src/app/synopsis/dummy-synopsis-objects.ts
@@ -2,5 +2,13 @@ import { SynopsisImageData, SynopsisObjectData, SynopsisTextData } from './synop
 
 export const DUMMYSYNOPSISOBJECTS: SynopsisObjectData[] = [
   new SynopsisTextData('A text', '<h3>A title...</h3><p>...with a text in <i>italic</i> and <b>bold</b>'),
-  new SynopsisImageData('A plane', '../../../assets/img/about/2.jpg')
+  new SynopsisTextData('Another text',
+    '<h3 style="color:red">Fancy title</h3><p style="text-decoration: underline wavy blue">A fancy text</p>'),
+  new SynopsisImageData('Team member #1', '../../../assets/img/team/1.jpg'),
+  new SynopsisImageData('Team member #2', '../../../assets/img/team/2.jpg'),
+  new SynopsisImageData('Team member #3', '../../../assets/img/team/3.jpg'),
+  new SynopsisImageData('El capitán', '../../../assets/img/about/1.jpg'),
+  new SynopsisImageData('A plane', '../../../assets/img/about/2.jpg'),
+  new SynopsisImageData('A laptop', '../../../assets/img/about/3.jpg'),
+  new SynopsisImageData('Mountains', '../../../assets/img/about/4.jpg')
 ];
--- src/app/synopsis/synopsis-text-object/synopsis-text-object.component.ts
+++ src/app/synopsis/synopsis-text-object/synopsis-text-object.component.ts
@@ -1,12 +1,21 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import {SynopsisObject} from '../synopsis-object';
 import {SynopsisTextData} from '../synopsis-object-data';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-synopsis-text-object',
   templateUrl: './synopsis-text-object.component.html',
   styleUrls: ['./synopsis-text-object.component.scss']
 })
-export class SynopsisTextObjectComponent implements SynopsisObject {
+export class SynopsisTextObjectComponent implements SynopsisObject, OnInit {
   @Input() data: SynopsisTextData;
+  safeHtml: SafeHtml;
+
+  constructor(private domSanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.safeHtml = this.domSanitizer.bypassSecurityTrustHtml(this.data.htmlText);
+  }
+
 }
