9ff4ff94cd74f1688602c9df42eeb25fbfd73d93
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-02-28 09:52:52 +0100

    added npm package as submodule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -11,13 +11,14 @@ import { MainComponent } from './main/main.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MetadataViewModule } from '../metadata-view/metadata-view.module';
-import { ArithmeticModule } from 'nie-ine';
-import {DashboardModule} from "../dashboard/dashboard.module";
-import {TextViewModule} from '../text-view/text-view.module';
-import {KnoraRequestService} from '../shared/knora-request.service';
-import {SparqlRequestService} from '../shared/sparql-request.service';
-import {ResultToTextMapperService} from '../text-view/result-to-text-mapper.service';
-import {KnoraAuthService} from '../shared/knora-auth.service';
+import { DashboardModule } from '../dashboard/dashboard.module';
+import { TextViewModule } from '../text-view/text-view.module';
+import { KnoraRequestService } from '../shared/knora-request.service';
+import { SparqlRequestService } from '../shared/sparql-request.service';
+import { ResultToTextMapperService } from '../text-view/result-to-text-mapper.service';
+import { KnoraAuthService } from '../shared/knora-auth.service';
+import { ArithmeticModule } from '../npm-package/src/modules/arithmetic.module';
+import { ExampleComponent } from '../npm-package/src/modules/exampleComponent/example.component';
 
 @NgModule({
   imports: [
@@ -29,8 +30,10 @@ import {KnoraAuthService} from '../shared/knora-auth.service';
     ArithmeticModule,
     GrapesjsModule,
     DashboardModule,
+    ArithmeticModule,
     RouterModule.forRoot([
-      {path: '**', component: PageNotFoundComponent}
+      { path: 'example', component: ExampleComponent },
+      { path: '**', component: PageNotFoundComponent }
       ])
   ],
   declarations: [HeaderComponent, FooterComponent, MainComponent, PageNotFoundComponent, NavigationComponent],
