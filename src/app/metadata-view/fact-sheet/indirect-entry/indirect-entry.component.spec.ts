cdedfc69fe1d89fe0464b6f13b31c3748339f18f
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2018-02-28 15:54:22 +0100

    make tests pass

--- src/app/metadata-view/fact-sheet/indirect-entry/indirect-entry.component.spec.ts
+++ src/app/metadata-view/fact-sheet/indirect-entry/indirect-entry.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IndirectEntryComponent } from './indirect-entry.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 describe('IndirectEntryComponent', () => {
   let component: IndirectEntryComponent;
@@ -9,8 +11,9 @@ describe('IndirectEntryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ RouterTestingModule ],
-      declarations: [ IndirectEntryComponent ]
+      imports: [ RouterTestingModule, HttpClientModule, HttpClientTestingModule ],
+      declarations: [ IndirectEntryComponent ],
+      providers: [ HttpClient ]
     })
     .compileComponents();
   }));
