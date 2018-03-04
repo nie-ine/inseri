47da8abad18e1e1f6f1fdf1a1f366f82d5fb5073
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-03-04 13:59:03 +0100

    Add validator to save light table dialog
    Signed-off-by: Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>

--- src/app/synopsis/synopsis-save-light-table/synopsis-save-light-table.component.ts
+++ src/app/synopsis/synopsis-save-light-table/synopsis-save-light-table.component.ts
@@ -1,7 +1,14 @@
 import {Component} from '@angular/core';
-import {MatDialogRef} from '@angular/material';
-import {FormControl} from '@angular/forms';
+import {ErrorStateMatcher, MatDialogRef} from '@angular/material';
+import {AbstractControl, FormControl, FormGroupDirective, NgForm, ValidatorFn, Validators} from '@angular/forms';
 import {SynopsisObjectSerializerService} from '../synopsis-object-serializer.service';
+
+export class InstantErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-synopsis-save-light-table',
@@ -10,10 +17,22 @@ import {SynopsisObjectSerializerService} from '../synopsis-object-serializer.ser
 })
 export class SynopsisSaveLightTableComponent {
 
-  filenameFormControl = new FormControl('');
+  filenameFormControl = new FormControl('', [
+    Validators.required,
+    this.lookupReservedFilenames(this.synopsisObjectSerializerService.getAllFilenames())
+  ]);
+
+  matcher = new InstantErrorStateMatcher();
 
   constructor(public dialogRef: MatDialogRef<SynopsisSaveLightTableComponent>,
               private synopsisObjectSerializerService: SynopsisObjectSerializerService) {
+  }
+
+  lookupReservedFilenames(reservedFilenames: string[]): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      return reservedFilenames.indexOf(control.value) > -1 ?
+        {'reservedFilename': {value: control.value}} : null;
+    };
   }
 
   saveAndCloseDialog() {
