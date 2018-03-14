fe40d52a495a5c10eeadd44ba2960c9cc6b55b4e
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-03-14 11:18:13 +0100

    Save tiled view permanently
    Signed-off-by: Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>

--- src/app/synopsis/synopsis-object-serializer.service.ts
+++ src/app/synopsis/synopsis-object-serializer.service.ts
@@ -3,27 +3,28 @@ import { SynopsisObjectStorageService } from './synopsis-object-storage.service'
 import { SynopsisObjectData } from './synopsis-object-data';
 import { Subject } from 'rxjs/Subject';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { LightTableLayoutService } from './light-table-layout.service';
 
 interface StorageObject {
-  [name: string]: [SynopsisObjectData[], SynopsisObjectData[]];
+  [name: string]: [SynopsisObjectData[], SynopsisObjectData[], boolean, number | undefined];
 }
 
 /**
- * Saves light table permanently
+ * In-memory storage for light table views
  */
 class LightTableStorage {
 
   private storage: StorageObject = {};
 
-  add(name: string, thumbnailSnapshot: SynopsisObjectData[], lightTableSnapshot: SynopsisObjectData[]) {
-    this.storage[name] = [thumbnailSnapshot, lightTableSnapshot];
+  add(name: string, thumbnailSnapshot: SynopsisObjectData[], lightTableSnapshot: SynopsisObjectData[], tiled: boolean, cols?: number) {
+    this.storage[name] = [thumbnailSnapshot, lightTableSnapshot, tiled, cols];
   }
 
   has(name: string): boolean {
     return Object.keys(this.storage).some(x => x === name);
   }
 
-  get(name: string): [SynopsisObjectData[], SynopsisObjectData[]] {
+  get(name: string): [SynopsisObjectData[], SynopsisObjectData[], boolean, number | undefined] {
     return this.storage[name];
   }
 
@@ -41,12 +42,17 @@ class LightTableStorage {
 
 }
 
+/**
+ * Saves light table permanently
+ */
 @Injectable()
 export class SynopsisObjectSerializerService {
   private thumbnailsSnapshot: Array<SynopsisObjectData>;
   private lightTableStorage = new LightTableStorage();
   private snapshotName: string;
   private sharedSnapshot = false;
+  private tiled;
+  private numberOfColumns;
 
   private makeLightTableSnapshotSource = new Subject();
   makeLightTableSnapshot$ = this.makeLightTableSnapshotSource.asObservable();
@@ -55,9 +61,12 @@ export class SynopsisObjectSerializerService {
   private propagateLightTableSharedSnapshotSource = new Subject<[SynopsisObjectData[], SynopsisObjectData[]]>();
   propagateLightTableSharedSnapshot$ = this.propagateLightTableSharedSnapshotSource.asObservable();
 
-  constructor(private synopsisObjectStorageService: SynopsisObjectStorageService) {
+  constructor(private synopsisObjectStorageService: SynopsisObjectStorageService,
+              private lightTableLayoutService: LightTableLayoutService) {
     synopsisObjectStorageService.synopsisObjects$
       .subscribe(objList => this.thumbnailsSnapshot = objList.map(x => Object.assign({}, x)));
+    lightTableLayoutService.numberOfColumns$.subscribe(cols => this.numberOfColumns = cols);
+    lightTableLayoutService.tiledLayout$.subscribe(tiled => this.tiled = tiled);
   }
 
   save(name: string): boolean {
@@ -76,6 +85,10 @@ export class SynopsisObjectSerializerService {
 
   load(name: string) {
     const snapshot = this.lightTableStorage.get(name);
+    this.lightTableLayoutService.tiledLayout(snapshot[2]);
+    if (snapshot[2]) {
+      this.lightTableLayoutService.numberOfColumns(snapshot[3]);
+    }
     this.synopsisObjectStorageService.replace(snapshot[0]);
     this.loadLightTableSnapshotSource.next(snapshot[1]);
   }
@@ -94,7 +107,13 @@ export class SynopsisObjectSerializerService {
       this.propagateLightTableSharedSnapshotSource.next([this.thumbnailsSnapshot, snapshot]);
       this.sharedSnapshot = false;
     } else {
-      this.lightTableStorage.add(this.snapshotName, this.thumbnailsSnapshot, snapshot);
+      this.lightTableStorage.add(
+        this.snapshotName,
+        this.thumbnailsSnapshot,
+        snapshot,
+        this.tiled,
+        this.tiled ? this.numberOfColumns : undefined
+      );
     }
   }
 
