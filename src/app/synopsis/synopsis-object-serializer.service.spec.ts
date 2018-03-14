4217c88a4e5cedf2b9af885df46b6f3cabc483c5
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-03-14 11:18:13 +0100

    Save tiled view permanently
    Signed-off-by: Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>

--- src/app/synopsis/synopsis-object-serializer.service.spec.ts
+++ src/app/synopsis/synopsis-object-serializer.service.spec.ts
@@ -2,11 +2,13 @@ import {inject, TestBed} from '@angular/core/testing';
 
 import {SynopsisObjectSerializerService} from './synopsis-object-serializer.service';
 import {SynopsisObjectStorageService} from './synopsis-object-storage.service';
+import { LightTableLayoutService } from './light-table-layout.service';
 
 describe('SynopsisObjectSerializerService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
+        LightTableLayoutService,
         SynopsisObjectSerializerService,
         SynopsisObjectStorageService
       ]
