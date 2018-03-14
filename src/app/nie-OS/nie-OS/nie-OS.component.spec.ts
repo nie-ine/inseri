d936ef4038d704173dbd3d8a204e8e8f50e8c5fc
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-03-14 14:32:50 +0100

    fixed test

--- src/app/nie-OS/nie-OS/nie-OS.component.spec.ts
+++ src/app/nie-OS/nie-OS/nie-OS.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GrapesjsComponent } from './grapesjs.component';
+import { NIEOSComponent } from './nie-OS.component';
 
 describe('GrapesjsComponent', () => {
-  let component: GrapesjsComponent;
-  let fixture: ComponentFixture<GrapesjsComponent>;
+  let component: NIEOSComponent;
+  let fixture: ComponentFixture<NIEOSComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GrapesjsComponent ]
+      declarations: [ NIEOSComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GrapesjsComponent);
+    fixture = TestBed.createComponent(NIEOSComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
