aed4da373374cfacda7e0f179e77edb5f35d08f6
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-03-26 16:19:07 +0200

    implemented links and first login function

--- src/app/static-pages/home/home.component.ts
+++ src/app/static-pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 declare let OpenSeadragon: any;
 
@@ -9,9 +10,25 @@ declare let OpenSeadragon: any;
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private fragment: string;
+
+  constructor( router: Router ) {
+    router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url);
+        if (tree.fragment) {
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(true); }
+        }
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  startLoginProcess() {
+    console.log('Start Login Process');
   }
 
 }
