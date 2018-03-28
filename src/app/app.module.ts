196a3128c5a0a0c7abd9dc127c8aedd699813ce9
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-03-28 11:19:06 +0200

    new try with search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
+import {AlertService} from "./shared/altert.service";
+import {AuthenticationService} from "./shared/authentication.service";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {JwtInterceptor} from "./shared/_helpers/jwt.interceptor";
+import {fakeBackendProvider} from "./shared/_helpers/fake-backend";
+import {AuthGuard} from "./shared/auth.guard";
+import {UserService} from "./shared/user.service";
 
 
 @NgModule({
@@ -10,10 +17,23 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-		BrowserModule,
-		CoreModule
+    BrowserModule,
+    CoreModule
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
