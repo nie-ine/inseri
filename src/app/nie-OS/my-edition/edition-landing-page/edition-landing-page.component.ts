9482cb74e72446ae1973c7963d8db95a4f534a9e
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-06-26 13:14:09 +0200

    switch to Salsah implemented

---
+++ src/app/nie-OS/my-edition/edition-landing-page/edition-landing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edition-landing-page',
+  templateUrl: './edition-landing-page.component.html',
+  styleUrls: ['./edition-landing-page.component.scss']
+})
+export class EditionLandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
