7f8325840e2f92ac33349a0ce650c19bd1d989c1
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-06-26 16:08:18 +0200

    implemented popup for adding a view

--- src/app/nie-OS/my-edition/edition-landing-page/edition-landing-page.component.ts
+++ src/app/nie-OS/my-edition/edition-landing-page/edition-landing-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Router, NavigationEnd } from '@angular/router';
+import { ActionService } from '../../../shared/action.service';
+import { AlertService} from '../../../shared/altert.service';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-edition-landing-page',
@@ -7,9 +12,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditionLandingPageComponent implements OnInit {
 
-  constructor() { }
+  animal: string;
+  name: string;
+
+  constructor(
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit() {
   }
 
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogCreateNewViewComponent, {
+      width: '700px',
+      data: { name: this.name, animal: this.animal }
+    });
+  }
+
+}
+
+@Component({
+  selector: 'dialog-create-new-view',
+  templateUrl: './dialog-create-new-view.html',
+})
+export class DialogCreateNewViewComponent {
+  model: any = {};
+  loading = false;
+  chooseNewAction: string;
+  constructor(public dialogRef: MatDialogRef<DialogCreateNewViewComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              private router: Router,
+              private actionService: ActionService) {
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  register() {
+    this.loading = true;
+    this.actionService.create(this.model)
+      .subscribe(
+        data => {
+          console.log('Action created');
+          const actions = JSON.parse(localStorage.getItem('actions')) || [];
+          console.log(actions);
+          this.onNoClick();
+          console.log(this.model.type.search('salsah'));
+          if ( this.model.type.search('salsah') !== -1 ) {
+            console.log('Navigate to Salsah');
+            window.open('http://salsah2.nie-ine.ch/', '_blank');
+          } else {
+            const params = new HttpParams().set('actionId', actions.lengt);
+            params.append('actionId', actions.length);
+            this.router.navigate( [ this.model.type ], { queryParams: { 'actionID': actions.length } } );
+          }
+        },
+        error => {
+          console.log(error);
+          this.loading = false;
+        });
+  }
 }
--- src/app/nie-OS/my-edition/my-edition.module.ts
+++ src/app/nie-OS/my-edition/my-edition.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EditionLandingPageComponent } from './edition-landing-page/edition-landing-page.component';
+import {
+  DialogCreateNewViewComponent,
+  EditionLandingPageComponent
+} from './edition-landing-page/edition-landing-page.component';
 import {RouterModule} from '@angular/router';
+import {MatButtonModule, MatDialogModule, MatIconModule, MatRadioModule} from "@angular/material";
+import {FormsModule} from "@angular/forms";
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatDialogModule,
+    FormsModule,
+    MatRadioModule,
+    MatMenuModule,
+    MatIconModule,
     RouterModule.forRoot([
       {path: 'my-edition', component: EditionLandingPageComponent}
     ])
   ],
   declarations: [
-    EditionLandingPageComponent
-  ]
+    EditionLandingPageComponent,
+    DialogCreateNewViewComponent
+  ],
+  entryComponents: [
+    DialogCreateNewViewComponent
+  ],
 })
 export class MyEditionModule { }
