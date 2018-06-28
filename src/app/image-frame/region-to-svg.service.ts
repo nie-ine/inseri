8f8cdf67360f454f9ecf2a65e31fe62c97878193
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2018-06-28 10:55:27 +0200

    reorganize (second part)

---
+++ src/app/image-frame/region-to-svg.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class RegionToSvgService {
+
+  constructor() { }
+
+  static createSvgElement(region, maxSide) {
+
+    if (region.type === 'polygon') {
+
+      const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+      svgElement.id = 'roi-svgoverlay-' + Math.random() * 10000;
+      let points = '';
+      for (let j = 0; j < region.points.length; j++) {
+        points += region.points[j].x;
+        points += ',';
+        points += region.points[j].y;
+        points += ' ';
+      }
+      svgElement.setAttribute('points', points);
+      svgElement.setAttribute('style', 'fill:' + region.lineColor + ';fill-opacity:0.2;stroke-width:' + region.lineWidth / maxSide + ';stroke:' + region.lineColor);
+
+      return svgElement;
+
+    } else if (region.type === 'rectangle') {
+
+      const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+      svgElement.id = 'roi-svgoverlay-' + Math.random() * 10000;
+      svgElement.setAttribute('width', (region.points[1].x - region.points[0].x).toString());
+      svgElement.setAttribute('height', (region.points[1].y - region.points[0].y).toString());
+      svgElement.setAttribute('x', region.points[0].x);
+      svgElement.setAttribute('y', region.points[0].y);
+      svgElement.setAttribute('style', 'fill:' + region.lineColor + ';fill-opacity:0.2;stroke-width:' + region.lineWidth / maxSide + ';stroke:' + region.lineColor);
+
+      return svgElement;
+
+    } else if (region.type === 'circle') {
+
+      const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      svgElement.id = 'roi-svgoverlay-' + Math.random() * 10000;
+      svgElement.setAttribute('cx', region.points[0].x);
+      svgElement.setAttribute('cy', region.points[0].y);
+      svgElement.setAttribute('r',
+        Math.sqrt(Math.pow(region.radius.x, 2) + Math.pow(region.radius.y, 2)).toString());
+      svgElement.setAttribute('stroke', region.lineColor);
+      svgElement.setAttribute('stroke-width', (region.lineWidth / maxSide).toString());
+      svgElement.setAttribute('fill', region.lineColor);
+      svgElement.setAttribute('style', 'fill-opacity:0.2;');
+
+
+      return svgElement;
+
+    } else {
+      return document.createElementNS('http://www.w3.org/2000/svg', 'title');
+    }
+  }
+
+
+}
---
+++ src/app/resource-form/resource-form/resource-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-form',
+  templateUrl: './resource-form.component.html',
+  styleUrls: ['./resource-form.component.scss']
+})
+export class ResourceFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
