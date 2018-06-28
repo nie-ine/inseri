ef3ffc948c5a03a92eec03210e1c707e49254939
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-06-28 14:53:09 +0200

    anpassen von titel, beschreibung und bild moeglich

---
+++ src/app/nie-OS/my-edition/update-edition/update-edition.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { ActionService } from '../../../shared/action.service';
+import {Router} from '@angular/router';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatFormField} from '@angular/material';
+
+@Component({
+  selector: 'app-update-edition',
+  templateUrl: './update-edition.component.html'
+})
+export class UpdateEditionComponent {
+
+  model: any = {};
+  loading = false;
+  chooseNewAction: string;
+  constructor(public dialogRef: MatDialogRef<UpdateEditionComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              private router: Router,
+              private actionService: ActionService) {
+    this.model = data;
+    console.log( data );
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  register() {
+    this.loading = true;
+    console.log('register');
+  }
+}
