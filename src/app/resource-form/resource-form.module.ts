0c7783981d04d66843466f94bd3fc1d110058168
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2018-07-02 12:13:45 +0200

    include editing fields for base types

--- src/app/resource-form/resource-form.module.ts
+++ src/app/resource-form/resource-form.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { BaseTypeFormsModule } from '../base-type-forms/base-type-forms.module';
 import { ResourceFormComponent } from './resource-form/resource-form.component';
 import { ResourceValueHistoryComponent } from './resource-value-history/resource-value-history.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    BaseTypeFormsModule
   ],
   declarations: [ResourceFormComponent, ResourceValueHistoryComponent ],
   exports: [ResourceFormComponent]
