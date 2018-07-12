97e0073860d2ad3500bfba298ee001244b2bf73d
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-07-12 14:43:09 +0200

    added data-chooser settings component

--- src/app/nie-OS/apps/data-management-tool/data-chooser/data-chooser.component.ts
+++ src/app/nie-OS/apps/data-management-tool/data-chooser/data-chooser.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import {Router} from '@angular/router';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatFormField} from '@angular/material';
+import {DataChooserSettingsComponent} from "../data-chooser-settings/data-chooser-settings.component";
 
 @Component({
   selector: 'app-data-chooser',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-chooser.component.scss']
 })
 export class DataChooserComponent implements OnInit {
-
-  constructor() { }
+  name: string;
+  animal: string;
+  constructor(
+    public dialogSettings: MatDialog
+  ) { }
 
   ngOnInit() {
+  }
+  openDataChooserDialog() {
+    console.log('Open Data Chooser Dialog');
+    const dialogRef = this.dialogSettings.open(DataChooserSettingsComponent, {
+      width: '700px',
+      data: { name: this.name, animal: this.animal }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
   }
 
 }
