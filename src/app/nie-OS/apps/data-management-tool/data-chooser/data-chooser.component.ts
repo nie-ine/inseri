3c3e8672d5bcc6cbe0bacc6cbc5def4004addef9
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-07-12 12:22:30 +0200

    added data chooser app

---
+++ src/app/nie-OS/apps/data-management-tool/data-chooser/data-chooser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-chooser',
+  templateUrl: './data-chooser.component.html',
+  styleUrls: ['./data-chooser.component.scss']
+})
+export class DataChooserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
