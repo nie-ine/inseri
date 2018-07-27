40f249ebdf763cc641252fdda2f958be21a02adb
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-07-27 15:25:59 +0200

    ready to work in view

--- src/app/nie-OS/apps/data-management-tool/data-chooser/data-chooser.component.ts
+++ src/app/nie-OS/apps/data-management-tool/data-chooser/data-chooser.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Inject, AfterViewChecked, ChangeDetectorRef} from '@a
 import {Router} from '@angular/router';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatFormField} from '@angular/material';
 import {DataChooserSettingsComponent} from "../data-chooser-settings/data-chooser-settings.component";
+import {SendGravSearchQueryService} from "../../../../shared/gravsearch/sendGravSearchQuery.service";
 
 @Component({
   selector: 'app-data-chooser',
@@ -9,11 +10,15 @@ import {DataChooserSettingsComponent} from "../data-chooser-settings/data-choose
   styleUrls: ['./data-chooser.component.scss']
 })
 export class DataChooserComponent implements OnInit, AfterViewChecked {
-  name: string;
-  animal: string;
+  gravSearchString: string;
+  dataChooserString: string;
+  gravSearchResponse: Array<any>;
+  chosenPropertyArray: Array<any>;
+  dataChooserSettingsOutput: any;
   constructor(
     public dialogSettings: MatDialog,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private sendGravSearchQueryService: SendGravSearchQueryService
   ) { }
 
   ngOnInit() {
@@ -23,14 +28,50 @@ export class DataChooserComponent implements OnInit, AfterViewChecked {
     const dialogRef = this.dialogSettings.open(DataChooserSettingsComponent, {
       width: '1000px',
       height: '1000px',
-      data: { name: this.name, animal: this.animal }
+      data: {  }
     });
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      if ( !result ) {
+        this.gravSearchString = 'PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>' +
+          'PREFIX incunabula: <http://0.0.0.0:3333/ontology/0803/incunabula/simple/v2#>' +
+          'CONSTRUCT { ?myVariable knora-api:isMainResource true .' +
+          '?myVariable incunabula:title ?title .' +
+          '?myVariable incunabula:description ?description .' +
+          '?myVariable incunabula:hasAuthor ?hasAuthor . }' +
+          'WHERE { ?myVariable incunabula:title ?title .' +
+          '?myVariable incunabula:description ?description .' +
+          '?myVariable incunabula:hasAuthor ?hasAuthor .' +
+          '?myVariable a incunabula:book . } '
+        this.dataChooserString = 'title';
+        this.performGravSearchQuery( this.gravSearchString );
+      } else {
+        this.dataChooserSettingsOutput = result;
+        console.log(result.gravSearchQuery);
+        result.gravSearchQuery.forEach((value: string, key: string) => {
+          this.gravSearchString = value;
+          this.performGravSearchQuery( this.gravSearchString );
+        });
+      }
     });
   }
   ngAfterViewChecked() {
     this.cdr.detectChanges();
   }
-
+  performGravSearchQuery( query: string ) {
+    this.sendGravSearchQueryService.sendRequest( query )
+      .subscribe(
+        data => {
+          this.gravSearchResponse = data['@graph'];
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  chooseResource(chosenResource: any) {
+    console.log('Chosen Resource:');
+    console.log(chosenResource);
+    console.log('Assign which app gets which input data and parse input data for each app');
+    console.log(this.dataChooserSettingsOutput.appModel);
+  }
 }
