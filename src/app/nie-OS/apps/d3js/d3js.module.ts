480ae302bbfd19d4fadb53b735ae2a5d81aa781a
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-08-20 13:55:34 +0200

    start of d3js tutorial

--- src/app/nie-OS/apps/d3js/d3js.module.ts
+++ src/app/nie-OS/apps/d3js/d3js.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { RouterModule } from "@angular/router";
+import { D3tutorialComponent } from './d3tutorial/d3tutorial.component';
+import { AceEditorModule } from 'ng2-ace-editor';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
-      { path: 'bar-chart', component: BarChartComponent }
-    ])
+      { path: 'bar-chart', component: BarChartComponent },
+      { path: 'd3-tutorial', component: D3tutorialComponent }
+    ]),
+    AceEditorModule
   ],
   declarations: [
-    BarChartComponent
+    BarChartComponent,
+    D3tutorialComponent
   ],
   exports: [
-    BarChartComponent
+    BarChartComponent,
+    D3tutorialComponent
 ]
 })
 export class D3jsModule { }
