3cfe018a65641f41e2a082ce18ba7689413fbade
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2018-09-07 14:31:43 +0200

    add chord diagram

--- src/app/nie-OS/nie-OS/nie-OS.component.ts
+++ src/app/nie-OS/nie-OS/nie-OS.component.ts
@@ -27,16 +27,15 @@ declare var grapesjs: any; // Important!
 export class NIEOSComponent implements OnInit, AfterViewChecked {
   showFiller = false;
   image = {
-    '@id' : 'http://rdfh.ch/kuno-raeber/Uzo2YDhzTr-8CUSg1pQL4Q/values/gJVf-AQjSbSTAo8EsU8ErQ',
+    '@id' : 'https://www.e-manuscripta.ch/zuz/i3f/v20/1510612/canvas/1510618',
     '@type' : 'knora-api:StillImageFileValue',
     'knora-api:fileValueAsUrl' :
-      'https://tools.wmflabs.org/' +
-      'zoomviewer/proxy.php?iiif=Lions_Family_Portrait_Masai_Mara.jpg/pct:65,81,35,15/full/0/default.jpg',
-    'knora-api:fileValueHasFilename' : 'proxy.php?iiif=Lions_Family_Portrait_Masai_Mara.jpg',
+      'https://www.e-manuscripta.ch/zuz/i3f/v21/1510618/full/1304/0/default.jpg',
+    'knora-api:fileValueHasFilename' : '1510618',
     'knora-api:fileValueIsPreview' : false,
-    'knora-api:stillImageFileValueHasDimX' : 5184,
-    'knora-api:stillImageFileValueHasDimY' : 3456,
-    'knora-api:stillImageFileValueHasIIIFBaseUrl' : 'https://tools.wmflabs.org/zoomviewer'
+    'knora-api:stillImageFileValueHasDimX' : 3062,
+    'knora-api:stillImageFileValueHasDimY' : 4034,
+    'knora-api:stillImageFileValueHasIIIFBaseUrl' : 'https://www.e-manuscripta.ch/zuz/i3f/v20'
   };
   projectIRI: string = 'http://rdfh.ch/projects/0001';
   actionID: number;
@@ -113,6 +112,10 @@ export class NIEOSComponent implements OnInit, AfterViewChecked {
     },
     stackedBarCharts: {
       type: 'stackedBarCharts',
+      model: []
+    },
+    chordDiagrams: {
+      type: 'chordDiagrams',
       model: []
     }
   };
@@ -199,6 +202,10 @@ export class NIEOSComponent implements OnInit, AfterViewChecked {
         },
         stackedBarCharts: {
           type: 'stackedBarCharts',
+          model: []
+        },
+        chordDiagrams: {
+          type: 'chordDiagrams',
           model: []
         }
       };
