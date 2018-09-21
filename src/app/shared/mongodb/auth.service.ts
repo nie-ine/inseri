7be7c00e2dce1d6b107fe5750656a7358aca250e
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-09-21 16:55:51 +0200

    added token

--- src/app/shared/mongodb/auth.service.ts
+++ src/app/shared/mongodb/auth.service.ts
@@ -4,8 +4,12 @@ import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-
+  private token: string;
   constructor( private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   createUser( email: string, password: string ) {
     const authData: AuthData = { email: email, password: password };
@@ -17,9 +21,12 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post('http://localhost:3000/api/user/login', authData)
+    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
       .subscribe( response => {
+        console.log('Loged in');
         console.log(response);
+        const token = response.token;
+        this.token = token;
       });
   }
 }
