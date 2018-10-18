31173846a9d3e1b58e403d68955c028c57e0540d
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-10-18 16:54:08 +0200

    added action route

---
+++ backend/routes/actions.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const Action = require('../models/action');
+
+const router = express.Router();
+
+router.post('', (req, res, next) => {
+  const action = req.body;
+  console.log(action);
+  res.status(201).json({
+    message: 'Action added successfully'
+  });
+});
+
+module.exports = router;
