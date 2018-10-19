6d84065e70599ca5f5e1e136c88391ecc7666efc
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-10-19 15:50:41 +0200

    dashboard loading actions from mongodb

--- backend/routes/action.js
+++ backend/routes/action.js
@@ -10,13 +10,25 @@ router.post('', (req, res, next) => {
   console.log('start action service in backend');
   const action = new Action({
     title: req.body.title,
-    description: req.body.description
+    description: req.body.description,
+    type: req.body.type
   });
   console.log(action);
   action.save();
   res.status(201).json({
     message: 'Action added successfully'
   });
+});
+
+router.get('', (req, res, next) => {
+  Action.find()
+    .then(documents => {
+      console.log(documents);
+      res.status(200).json({
+        message: 'action returned in response',
+        actions: documents
+      });
+    });
 });
 
 module.exports = router;
