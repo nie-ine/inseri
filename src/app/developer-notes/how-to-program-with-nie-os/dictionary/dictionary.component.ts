afd5dac7d027e17def83129211f5254e9d77fea4
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-10-26 16:45:28 +0200

    almost done

--- src/app/developer-notes/how-to-program-with-nie-os/dictionary/dictionary.component.ts
+++ src/app/developer-notes/how-to-program-with-nie-os/dictionary/dictionary.component.ts
@@ -5,13 +5,23 @@ import {Component, Input, OnChanges, OnInit} from '@angular/core';
   templateUrl: './dictionary.component.html',
   styleUrls: ['./dictionary.component.scss']
 })
-export class DictionaryComponent implements OnChanges {
+export class DictionaryComponent implements OnInit {
 
   @Input() dictionary: any;
+  dictionaryArray: Array<any> = [];
   constructor() { }
 
-  ngOnChanges() {
+  ngOnInit() {
     console.log(this.dictionary);
+    for ( const entry in this.dictionary ) {
+      console.log(entry);
+      console.log(this.dictionary[ entry ]);
+      const length = this.dictionaryArray.length;
+      this.dictionaryArray[ length ] = [];
+      this.dictionaryArray[ length ].name = this.dictionary[ entry ].name;
+      this.dictionaryArray[ length ].description = this.dictionary[ entry ].description;
+      console.log(this.dictionaryArray);
+    }
   }
 
 }
