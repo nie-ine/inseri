c6213982a3e3272c37b3e408118e3e4fad6ee79e
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2018-10-29 16:58:31 +0100

    start svg text view with interaction with page transcription

--- src/app/nie-OS-apps/complex-text-views/complex-text-views.module.ts
+++ src/app/nie-OS-apps/complex-text-views/complex-text-views.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TextPageOfWordsComponent } from './text-page-of-words/text-page-of-words.component';
 import { TextStructureModule } from '../text-structure/text-structure.module';
 import { SpaTextModule } from '../shared/spa-text/spa-text.module';
+import { TextSvgViewComponent } from './text-svg-view/text-svg-view.component';
 
 @NgModule({
   imports: [
@@ -10,7 +11,7 @@ import { SpaTextModule } from '../shared/spa-text/spa-text.module';
     SpaTextModule,
     TextStructureModule
   ],
-  declarations: [TextPageOfWordsComponent],
-  exports: [TextPageOfWordsComponent]
+  declarations: [TextPageOfWordsComponent, TextSvgViewComponent],
+  exports: [TextPageOfWordsComponent, TextSvgViewComponent]
 })
 export class ComplexTextViewsModule { }
