726fe227e0dc599f9134f752ca1e6943fdeed549
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2018-11-01 16:52:10 +0100

    preparing pageSet request for mongoDB

--- backend/routes/page-set.js
+++ backend/routes/page-set.js
@@ -8,15 +8,34 @@ const checkAuth = require("../middleware/check-auth");
 const router = express.Router();
 
 router.post('', (req, res, next) => {
-    console.log('Method: POST, Url: /pageset')
+    console.log('Method: POST, Url: /pages');
+    res.status(200).json({
+      message: 'PageSet POST',
+    });
   });
 
 router.get('', (req, res, next) => {
-    console.log('Method: GET, Url: /pageset')
+  res.status(200).json({
+      message: 'PageSet GET',
   });
+});
+
+router.get('/:hash', (req, res, next) => {
+  res.status(200).json({
+    message: 'PageSet GET ' + req.params.hash,
+  });
+});
+
+router.put('/:id', (req, res, next) => {
+  res.status(200).json({
+    message: 'PageSet PUT ' + req.params.id,
+  });
+});
 
 router.delete('/:id', (req, res, next) => {
-  console.log('Method: DELETE, Url: /pageset/id')
+  res.status(200).json({
+      message: 'PageSet DELETE ' + req.params.id,
+  });
 });
 
 module.exports = router;
--- src/app/nie-OS/page-set/model/page-set.service.ts
+++ src/app/nie-OS/page-set/model/page-set.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-
 import { PageSetModel } from './page-set.model';
-
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class PageSetService {
+  private static API_BASE_URL = 'http://localhost:3000/api/pages';
+
   constructor(private http: HttpClient) { }
 
   getAll() {
@@ -26,5 +27,27 @@ export class PageSetService {
 
   delete(hash: string) {
     return this.http.delete('/api/pages/' + hash);
+  }
+
+  // Request for the MongoDB
+
+  createPageSet(pageSet: PageSetModel): Observable<any> {
+    return this.http.post(`${PageSetService.API_BASE_URL}`, pageSet);
+  }
+
+  getPageSet(hash: string): Observable<any> {
+    return this.http.get(`${PageSetService.API_BASE_URL}/${hash}`);
+  }
+
+  getAllPageSet(): Observable<any> {
+    return this.http.get(`${PageSetService.API_BASE_URL}`);
+  }
+
+  updatePageSet(pageSetId: number, pageSet: PageSetModel): Observable<any> {
+    return this.http.put(`${PageSetService.API_BASE_URL}/${pageSetId}`, pageSet);
+  }
+
+  deletePageSet(hash: string): Observable<any> {
+    return this.http.delete(`${PageSetService.API_BASE_URL}/${hash}`);
   }
 }
