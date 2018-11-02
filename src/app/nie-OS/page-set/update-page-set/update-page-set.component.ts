f996ae71518fa79155935cbdfc13c939e355a064
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2018-11-02 15:07:33 +0100

    clean up edit page-set dialog

--- src/app/nie-OS/page-set/update-page-set/update-page-set.component.ts
+++ src/app/nie-OS/page-set/update-page-set/update-page-set.component.ts
@@ -1,34 +1,21 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { ActionService } from '../../../shared/nieOS/fake-backend/action/action.service';
-import {Router} from '@angular/router';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatFormField} from '@angular/material';
+import {Component, Inject } from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-update-page-set',
-  templateUrl: './update-page-set.component.html'
+  templateUrl: './update-page-set.component.html',
+  styleUrls: ['./update-page-set.component.scss']
 })
 export class UpdatePageSetComponent {
 
-  model: any = {};
-  loading = false;
-  chooseNewAction: string;
-
-  constructor(
-    public dialogRef: MatDialogRef<UpdatePageSetComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    private router: Router,
-    private actionService: ActionService
-  ) {
-    this.model = data;
-    console.log( data );
+  constructor(public dialogRef: MatDialogRef<UpdatePageSetComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
   }
 
-  onNoClick(): void {
+  save() {
+    this.dialogRef.close(this.data);
+  }
+
+  cancel() {
     this.dialogRef.close();
-  }
-
-  register() {
-    this.loading = true;
-    console.log('register');
   }
 }
