2e76afa872e53955243c975de8bd38e901dcc581
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-11-26 17:01:36 +0100

    open apps console.logged in app query table

--- src/app/nie-OS/query-entry/query-entry.component.ts
+++ src/app/nie-OS/query-entry/query-entry.component.ts
@@ -14,33 +14,47 @@ export class QueryEntryComponent {
   server = 'http://0.0.0.0:3333/';
   firstStep = 'admin/projects?email=root%40example.com&password=test';
   response: any;
+  tree: any;
   abstractJson: any;
   constructor(
     public dialogRef: MatDialogRef<QueryEntryComponent>,
-    @Inject(MAT_DIALOG_DATA) public data1: any,
+    @Inject(MAT_DIALOG_DATA) public inputQuery: any,
     private http: HttpClient,
     private abstractJsonService: AbstractJsonService
-  ) { }
+  ) {
+  }
 
   close() {
-    this.dialogRef.close();
+    const output = [
+      this.abstractJson,
+      this.inputQuery
+    ]
+    this.dialogRef.close(
+      output
+    );
   }
 
   sendQuery() {
-    console.log( 'Send query' );
+    // console.log( 'Send query' );
     const queryToBeSend = this.server + this.firstStep;
-    console.log( queryToBeSend );
+    // console.log( queryToBeSend );
     this.http.get( queryToBeSend )
       .subscribe(
         data => {
-          console.log( data );
+          // console.log( data );
           this.response = data;
+          this.tree = data;
           this.abstractJson = this.abstractJsonService.json2abstract( data );
           console.log( this.abstractJson );
         }, error => {
           console.log( error );
         }
       );
+  }
+
+  changeTreeInput( tree: any ) {
+    console.log( 'Change tree', tree );
+    this.tree = tree;
   }
 
 }
