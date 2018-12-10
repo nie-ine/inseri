0e02a0217df1f730050023d8e283805aebb0e95b
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-12-10 15:03:05 +0100

    generating path almost working

--- src/app/nie-OS/apps/data-management-tool/response-tree/generate-path.service.ts
+++ src/app/nie-OS/apps/data-management-tool/response-tree/generate-path.service.ts
@@ -4,12 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GeneratePathService {
-
+  path = [];
   constructor() { }
 
   generatePath( hash: string, tree: any ) {
     console.log( 'Generate Path for', hash, tree );
+    this.iterateThroughTree( tree, hash );
+    console.log( tree );
     return hash;
   }
 
+  iterateThroughTree( tree: any, hash: string ) {
+    for ( const leaf in tree ) {
+      if ( typeof tree[ leaf ] === 'object' && leaf !== 'path' ) {
+        if ( tree[ leaf ].hash === hash ) {
+          console.log( 'found hash', tree[ leaf ] );
+          this.path = [];
+          this.path.push( tree[ leaf ].parent, leaf );
+          this.iterateBack();
+        } else {
+          this.iterateThroughTree( tree[ leaf ], hash );
+        }
+      }
+    }
+  }
+
+  iterateBack() {
+    console.log( 'path:', this.path );
+  }
 }
