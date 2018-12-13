90734b9458f6e620bb9fa219284bca494200ddb5
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2018-12-13 10:12:33 +0100

    paths successfully saved! yay

--- src/app/nie-OS/query-app-input-map/query-app-input-map.component.ts
+++ src/app/nie-OS/query-app-input-map/query-app-input-map.component.ts
@@ -3,6 +3,7 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {query} from '@angular/animations';
 import {HttpClient} from '@angular/common/http';
 import {AbstractJsonService} from '../data-management/abstract-json.service';
+import {MongoPageService} from '../../shared/nieOS/mongodb/page/page.service';
 
 
 @Component({
@@ -16,22 +17,27 @@ export class QueryAppInputMapComponent implements OnInit {
   abstractResponse: any;
   chosenInputs: Array<any> = [];
   mapping: any;
-  paths: Array<any>;
+  paths: Array<any> = [];
   mappingUsedByPage: any = {};
   constructor(
     public dialogRef: MatDialogRef<QueryAppInputMapComponent>,
     @Inject(MAT_DIALOG_DATA) public input: any,
     private http: HttpClient,
-    private abstractJsonService: AbstractJsonService
+    private abstractJsonService: AbstractJsonService,
+    private pageService: MongoPageService
   ) {
     // if ( !this.input ) {
     //   this.input = JSON.parse(localStorage.getItem('mapInputs'));
     //   console.log( this.input );
     // }
     for ( const appInput in input.mapping[ input.app.hash ] ) {
-      this.chosenInputs.push( appInput );
+      console.log( this.input.mapping[ input.app.hash ][appInput] );
+      if ( this.input.mapping[ input.app.hash ][appInput][ 'query' ] === this.input.query._id ) {
+        this.chosenInputs.push( appInput );
+        this.paths[ appInput ] = this.input.mapping[ input.app.hash ][appInput][ 'path' ];
+      }
     }
-    console.log( this.input, 'chosenInputs:', this.chosenInputs );
+    console.log( this.paths );
   }
 
   ngOnInit() {
@@ -71,13 +77,17 @@ export class QueryAppInputMapComponent implements OnInit {
   }
 
   save() {
-    console.log(
-      'save paths to appInputQueryMapping',
-      this.input,
-      this.paths
-    );
     for ( const appInput in this.paths ) {
-      console.log( appInput, this.paths[ appInput ], this.input.page.appInputQueryMapping );
+      this.input.page.appInputQueryMapping[ this.input.app.hash ][ appInput ][ 'path' ] = this.paths[ appInput ];
     }
+    console.log(this.input.page);
+    this.pageService.updatePage(this.input.page)
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 }
