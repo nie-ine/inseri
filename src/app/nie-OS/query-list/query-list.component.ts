73c0c9b794792ddacf12724f0ce407c54b69cd82
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2019-01-09 17:00:32 +0100

    First look at the list of queries

--- src/app/nie-OS/query-list/query-list.component.ts
+++ src/app/nie-OS/query-list/query-list.component.ts
@@ -8,13 +8,25 @@ import { QueryService } from '../../shared/nieOS/mongodb/query/query.service';
   styleUrls: ['./query-list.component.scss']
 })
 export class QueryListComponent implements OnInit {
+  queries: any;
+  displayedColumns = ['add', 'title', 'delete'];
 
   constructor(public dialogRef: MatDialogRef<QueryListComponent>, private queryService: QueryService) { }
 
   ngOnInit() {
-    this.queryService.getAllQueriesOfUser(localStorage.getItem('userId')).subscribe(data => console.log(data));
-    this.queryService.getQuery('5c07dbfdff37d7344e1e1479').subscribe(query => console.log(query));
-    this.queryService.getAllQueries().subscribe(data => console.log(data));
+    this.queryService.getAllQueriesOfUser(localStorage.getItem('userId')).subscribe(data => {
+      this.queries = data.queries;
+      console.log(data.queries);
+    });
+    // this.queryService.getQuery('5c07dbfdff37d7344e1e1479').subscribe(query => console.log(query));
+  }
+
+  add(item: any) {
+    console.log(item);
+  }
+
+  openQueryEntry(item: any) {
+
   }
 
   close() {
