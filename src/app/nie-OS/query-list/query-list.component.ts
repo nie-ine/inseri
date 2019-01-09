50b42bb843da494f004f0ac6cff62d0db8ecc2c3
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2019-01-09 13:35:33 +0100

    Test getting all queries

--- src/app/nie-OS/query-list/query-list.component.ts
+++ src/app/nie-OS/query-list/query-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
+import { MongoQueryService } from '../../shared/nieOS/mongodb/query/query.service';
 
 @Component({
   selector: 'app-query-list',
@@ -8,9 +9,11 @@ import { MatDialogRef } from '@angular/material';
 })
 export class QueryListComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<QueryListComponent>) { }
+  constructor(public dialogRef: MatDialogRef<QueryListComponent>, private queryService: MongoQueryService) { }
 
   ngOnInit() {
+    this.queryService.getAllQueries().subscribe(data => console.log(data));
+    this.queryService.getQuery('5c07dbfdff37d7344e1e1479').subscribe(query => console.log(query));
   }
 
   close() {
