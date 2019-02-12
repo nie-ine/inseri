088412ac5c36bff16da68dfc03e54e6ecd4d31f5
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2019-02-12 13:13:24 +0100

    closes #151

--- src/app/query-app-interface/data-management/response-tree/generate-path.service.ts
+++ src/app/query-app-interface/data-management/response-tree/generate-path.service.ts
@@ -34,8 +34,9 @@ export class GeneratePathService {
   iterateBack( tree: any, hash: string ) {
     // console.log( 'path:', this.path );
     if (
-      tree[ this.path[ 0 ] ] &&
-      ( !(this.path.length > 1) || tree[ this.path[ 0 ] ][ this.path[ 1 ] ] )
+      this.checkIfPathExists( this.path, tree, 0 )
+      // tree[ this.path[ 0 ] ] &&
+      // ( !(this.path.length > 1) || tree[ this.path[ 0 ] ][ this.path[ 1 ] ] )
     ) {
       // console.log( 'path complete', this.path );
       return this.path;
@@ -43,9 +44,10 @@ export class GeneratePathService {
       for ( const leaf in tree ) {
         if ( typeof tree[ leaf ] === 'object' && leaf !== 'path' ) {
           if (
-            ( tree[ leaf ][ this.path[ 0 ] ] ) &&
-            ( !(this.path.length > 1) ||  tree[ leaf ][ this.path[ 0 ] ][ this.path[ 1 ] ] ) &&
-            ( !(this.path.length > 2) ||  tree[ leaf ][ this.path[ 0 ] ][ this.path[ 1 ] ][ this.path[ 2 ] ] )
+            // ( tree[ leaf ][ this.path[ 0 ] ] ) &&
+            // ( !(this.path.length > 1) ||  tree[ leaf ][ this.path[ 0 ] ][ this.path[ 1 ] ] ) &&
+            // ( !(this.path.length > 2) ||  tree[ leaf ][ this.path[ 0 ] ][ this.path[ 1 ] ][ this.path[ 2 ] ] )
+            this.checkIfPathExists( this.path, tree[ leaf ], 0 )
           ) {
             this.path.splice(0, 0, leaf );
             this.iterateBack( this.tree, hash );
@@ -55,6 +57,18 @@ export class GeneratePathService {
           }
         }
       }
+    }
+  }
+
+  checkIfPathExists( path: any, subtree: any, depth: number ): boolean {
+    if ( !(path.length > depth ) || subtree[ path[ depth ] ] ) {
+      if ( path.length < depth ) {
+        this.checkIfPathExists( path, subtree, depth + 1 );
+      } else {
+        return true;
+      }
+    } else {
+      return false;
     }
   }
 }
