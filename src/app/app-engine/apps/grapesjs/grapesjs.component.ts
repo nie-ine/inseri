95590d1d4d6eca33d5a3b19983ec1e4208933677
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2019-02-25 13:54:12 +0100

    any changes?

---
+++ src/app/app-engine/apps/grapesjs/grapesjs.component.ts
@@ -0,0 +1,31 @@
+import {AfterViewChecked, Component, OnInit} from '@angular/core';
+import 'rxjs/add/operator/map'
+
+declare var grapesjs: any; // Important!
+
+
+@Component({
+  selector: 'app-grapesjs',
+  template: '<div id="grapesJSViewer"></div>',
+  styleUrls: ['./grapesjs.component.scss']
+})
+export class GrapesjsComponent implements AfterViewChecked {
+
+  constructor() { }
+
+  ngAfterViewChecked() {
+    const editor = grapesjs.init({
+      container: '#grapesJSViewer',
+      components: '<div class="txt-red">Hello world!</div>',
+      style: '.txt-red{color: red}',
+    });
+
+    const blockManager = editor.BlockManager;
+
+    blockManager.add('konvolut-titel', {
+      label: 'Vokabulare in Knora',
+      attributes: { class:'fa fa-newspaper-o' },
+      content:  '<div>NIE-INE test</div>',
+    });
+  }
+}
