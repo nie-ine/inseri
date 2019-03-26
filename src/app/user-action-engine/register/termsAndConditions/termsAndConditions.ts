894c4f061102cf5671d715be5b2367d0b5a712ce
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2019-03-26 15:01:29 +0100

    terms and conditions

---
+++ src/app/user-action-engine/register/termsAndConditions/termsAndConditions.ts
@@ -0,0 +1,68 @@
+export class TermsAndConditions {
+  nutzungsbedingungen =
+  '<h6>Gegenstand</h6>' +
+  '<span>' + 'Die Nationale Infrastruktur für Editionen – ' +
+    'Infrastructure nationale pour les éditions (NIE – INE) ' +
+    'bietet eine in der Schweiz entwickelte Arbeits- und Publikationsplattform, ' +
+    'um insbesondere die langfristige Sicherung, Verfügbarkeit und Publikation ' +
+    'digitaler Forschungsergebnisse und der zugrundeliegenden Forschungsdaten ' +
+    'komplexer wissenschaftlicher digitaler Editionen zu gewährleisten. ' +
+    'Gleichzeitig ermöglicht die Plattform für laufende und künftige Editionsprojekte ' +
+    'die Erstellung einer digitalen Ausgabe. Zur Garantie der langfristigen Datenverfügbarkeit ' +
+    'und -serialisierung nutzt die NIE–INE die bestehende Infrastruktur des Data and Service Center ' +
+    'for the Humanities (DaSCH), einer Unternehmung der SAGW mit dem Auftrag, geisteswissenschaftliche ' +
+    'digitale Forschungsergebnisse langfristig zu sichern und permanent zugänglich zu halten.' +
+    '</span>' + '<br><br><span>Die NIE-Plattform ist Teil der NIE-INE und ermöglicht den Editionsprojekten, … </span> <br><br>' +
+    '<ul>' +
+    '  <li>mit eigenen Forschungsdaten, sowie mit von anderen Anbietern über die Plattform ' +
+    'bereitgestellten Forschungsdaten arbeiten zu können,</li>' +
+    '  <li>mit Ansichtsvariationen und Suchfunktionen ihre Daten anzuzeigen und für eine Publikation aufzubereiten,</li>' +
+    '  <li>sich mithilfe bereitgestellter Komponenten eine individuelle Arbeitsumgebung einzurichten</li>' +
+    '<li>diese individuelle Arbeitsumgebung und Forschungsdatenpräsentation abzuspeichern' +
+    'und zu einem anderen Zeitpunkt daran weiterzuarbeiten und</li>' +
+    '<li>in Absprache mit dem DaSCH, das Erarbeitete (oder einen Teil davon) zu publizieren und zu archivieren,' +
+    'vergleiche hierzu die Services des DaSCH. Eine Änderung des Publizierten ist nach der Publikation nicht mehr möglich,' +
+    'da die darin enthaltenen Forschungsdaten über persistente Identifikatoren zitierbar gemacht werden. Korrekturen können deshalb' +
+    'nur durch Veröffentlichung einer neuen Version erfolgen. Momentan ist das Publizieren nur für die an dem Projekt' +
+    'direkt beteiligten wissenschaftlichen Editionsprojekte möglich. Anderweitige Nutzungen sind nur in Absprache und mit' +
+    'schriftlicher Bestätigung durch die NIE-INE möglich.</li>' +
+    '</ul>' +
+    '<h6>Teilnahme in der Plattform</h6>' +
+    '<span>Um die Plattform und die oben beschriebenen Dienste nutzen zu können, ' +
+    'ist die Einrichtung eines persönlichen Benutzungskontos notwendig. Der Umgang ' +
+    'mit den entsprechend erhobenen personenbezogenen Daten ist in der Datenschutzerklärung beschrieben.</span> <br>' +
+    '<span>Die persönlichen Zugangsdaten zur NIE-Plattform dürfen Dritten nicht zur Nutzung überlassen werden.</span>' +
+    '<h6>Rechte und Pflichten von NIE-INE</h6>' +
+    '<span>Die NIE-INE verpflichtet sich zu einer reibungslosen Zugänglichkeit der bereitgestellten Plattform. ' +
+    'Durch Wartungsarbeiten, Weiterentwicklung oder Störungen können Zugangseinschränkungen auftreten. ' +
+    'Die Betreiber der Plattform führen eine sorgfältige Sicherung der Forschungsdaten durch. ' +
+    'Die Daten werden in mehreren Kopien an mindestens zwei geographisch verschiedenen ' +
+    'Serverstandorten in der Schweiz gespeichert. </span> <br><br>' +
+    '<span>Der Anbieter NIE-INE ist berechtigt, bei Verstössen gegen diese Nutzungsbedingungen ' +
+    'durch Nutzende oder bei Verstoss gegen das Gesetz den Zugang des Nutzenden zu sperren. ' +
+    'Der Nutzende wird darüber per Mail informiert. Erhebt er keinen Einspruch innerhalb ' +
+    'von 6 Wochen nach Absenden der Mail, werden sein Konto und die darin erstellten ' +
+    'Inhalte nach Ablauf dieser Frist gelöscht.</span><br>' +
+    '<h6>Beendigung der Teilnahme</h6>' +
+    '<span>Nutzende können ihre Teilnahme durch eine entsprechende Erklärung gegenüber der ' +
+    'NIE-INE ohne Einhaltung einer Frist beenden, woraufhin das Konto gelöscht wird.</span><br><br>' +
+    '<span>Für die Sicherung seiner Forschungsdaten ist der Nutzende vor Beendigung der Teilnahme selbst verantwortlich.</span>' +
+    '<h6>Änderungen der Nutzungsbedingungen</h6>' +
+    '<span>NIE-INE behält sich das Recht vor, diese Nutzungsbedingungen bei Bedarf anzupassen. ' +
+    'Über jede Änderung der Nutzungsbedingungen werden die Nutzenden informiert.</span>' +
+    '<h6>Haftung und anwendbares Recht</h6>' +
+    '<span>Die NIE-INE übernimmt keine Verantwortung für die Inhalte der NIE-Plattform.</span><br><br>' +
+    '<span>Ausser für vorsätzlichen oder grobfahrlässigen Datenverlust, verursacht durch NIE-INE oder durch ' +
+    'das DaSCH, wird jegliche Haftung abgelehnt.\n</span><br><br>' +
+    '<span>Die Einhaltung der Rechte Dritter bei der Bearbeitung von Forschungsdaten als auch bei deren Publikation ' +
+    'liegt in der Verantwortung des Nutzenden. Die Nutzenden stellen die NIE-INE von jeglichen Ansprüchen infolge ' +
+    'Verletzung von Urheber-, Datenschutz- oder anderen Immaterialgüterrechten Dritter frei.</span><br><br> ' +
+    '<span>Die von anderen Anbietern über die Plattform freigegebenen Forschungsdaten können urheberrechtlich ' +
+    'geschützt sein. Sie dürfen nur innerhalb der Plattform bearbeitet und nicht exportiert und (nicht ' +
+    'ohne vorherige Rechtsabklärung) publiziert werden. Die NIE-INE übernimmt keine Haftung für allfällig ' +
+    'widerrechtliche Verwendung von Daten.</span><br><br>' +
+    '<span>Diese Nutzungsbedingungen und die Nutzung der NIE-Plattform unterliegen ' +
+    'dem schweizerischen Recht. Mit der Nutzung der NIE-Plattform und Ihrem Einverständnis mit ' +
+    'diesen Bedingungen unterwerfen Sie sich der ausschliesslichen Gerichtsbarkeit der Gerichte am Sitz Basel.</span><br><br>'
+    ;
+}
