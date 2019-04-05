8dbf81d7849e0fa76992bdbf81a22845e93a65cb
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2019-04-05 15:30:36 +0200

    add component for the first project

---
+++ src/app/app-engine/apps/project-specific/project-0041/project-0041.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { P0041EditionComponent } from './p0041-edition/p0041-edition.component';
+import { P0041StropheComponent } from './p0041-strophe/p0041-strophe.component';
+import { ComplexTextViewsModule } from '../../complex-text-views/complex-text-views.module';
+import { TextStructureModule } from '../../text-structure/text-structure.module';
+import { SpaTextModule } from '../../shared/spa-text/spa-text.module';
+
+@NgModule({
+  declarations: [P0041EditionComponent, P0041StropheComponent],
+  imports: [
+    CommonModule,
+    ComplexTextViewsModule,
+    TextStructureModule,
+    SpaTextModule
+  ],
+  exports: [ P0041EditionComponent ]
+})
+export class Project0041Module { }
