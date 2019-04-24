830d9b68e4fec7b92848a3baab1561538bc0842b
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2019-04-24 10:40:32 +0200

    #240 fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   name: 'nieOS',
   api: 'http://localhost:3333',
-  app: 'http://localhost:4200',
+  app: 'https://nieos.nie-ine.ch',
   media: 'http://localhost:1024',
   node: 'https://node.nie-ine.ch'
 };
