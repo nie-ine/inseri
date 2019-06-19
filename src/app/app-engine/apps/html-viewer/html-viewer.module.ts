3834336d8cfbcb2e6386b5e667b6f7ed4dd96af4
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2019-06-19 15:45:20 +0200

    export rich HTML viewer into app

---
+++ src/app/app-engine/apps/html-viewer/html-viewer.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HtmlViewerComponent } from './html-viewer/html-viewer.component';
+import { RichTextModule } from '../shared/rich-text/rich-text.module';
+
+@NgModule({
+  declarations: [HtmlViewerComponent],
+  imports: [
+    CommonModule,
+    RichTextModule
+  ]
+})
+export class HtmlViewerModule { }
