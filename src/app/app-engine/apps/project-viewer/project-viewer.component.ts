c9a982b63e42376857ed5b03fb7216dd9f7aa842
André Kilchenmann <github@milchkannen.ch>
2019-06-27 11:31:57 +0200

    feat(knora-ui): Add @knora/core service to get project information

--- src/app/app-engine/apps/project-viewer/project-viewer.component.ts
+++ src/app/app-engine/apps/project-viewer/project-viewer.component.ts
@@ -2,30 +2,43 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Project, ProjectsService, ApiServiceError } from '@knora/core';
 
 @Component({
-  selector: 'app-project-viewer',
-  templateUrl: './project-viewer.component.html',
-  styleUrls: ['./project-viewer.component.scss']
+    selector: 'app-project-viewer',
+    templateUrl: './project-viewer.component.html',
+    styleUrls: ['./project-viewer.component.scss']
 })
 export class ProjectViewerComponent implements OnInit, OnChanges {
 
-  // @Input() iri: string = '';
+    @Input() projectToDisplay?: string = 'http://rdfh.ch/projects/0001';
 
-  projects: Project[];
+    projects: Project[];
 
-  constructor (private _projectsService: ProjectsService) { }
+    project: Project;
 
-  ngOnChanges() { }
+    constructor (private _projectsService: ProjectsService) { }
 
-  ngOnInit() {
+    ngOnChanges() { }
 
-    this._projectsService.getAllProjects().subscribe(
-      (results: Project[]) => {
-        this.projects = results;
-      },
-      (error: ApiServiceError) => {
-        console.error(error);
-      }
-    );
-  }
+    ngOnInit() {
+
+        this._projectsService.getProjectByIri(this.projectToDisplay).subscribe(
+            (results: Project) => {
+                this.project = results;
+            },
+            (error: ApiServiceError) => {
+                console.error(error);
+            }
+        );
+
+        /*
+        this._projectsService.getAllProjects().subscribe(
+            (results: Project[]) => {
+                this.projects = results;
+            },
+            (error: ApiServiceError) => {
+                console.error(error);
+            }
+        );
+        */
+    }
 
 }
--- src/app/user-action-engine/mongodb/page/open-apps.model.ts
+++ src/app/user-action-engine/mongodb/page/open-apps.model.ts
@@ -1,178 +1,183 @@
 export class OpenAppsModel {
-  openApps = {
-    imageViewer: {
-      type: 'imageViewer',
-      model: [],
-      inputs: [
-        {
-          'inputName': '@id',
+    openApps = {
+        imageViewer: {
+            type: 'imageViewer',
+            model: [],
+            inputs: [
+                {
+                    'inputName': '@id',
+                },
+                {
+                    'inputName': '@type',
+                },
+                {
+                    'inputName': 'fileValueAsUrl',
+                },
+                {
+                    'inputName': 'fileValueHasFilename',
+                },
+                {
+                    'inputName': 'fileValueIsPreview',
+                },
+                {
+                    'inputName': 'stillImageFileValueHasDimX',
+                },
+                {
+                    'inputName': 'stillImageFileValueHasDimY',
+                },
+                {
+                    'inputName': 'stillImageFileValueHasIIIFBaseUrl',
+                }
+            ]
         },
-        {
-          'inputName': '@type',
+        textViewer: {
+            type: 'textViewer',
+            model: []
         },
-        {
-          'inputName': 'fileValueAsUrl',
+        projectViewer: {
+            type: 'projectViewer',
+            model: [],
+            inputs: [
+                {
+                    'inputName': 'iri'
+                }
+            ]
         },
-        {
-          'inputName': 'fileValueHasFilename',
+        searchViewer: {
+            type: 'searchViewer',
+            model: []
         },
-        {
-          'inputName': 'fileValueIsPreview',
+        grapesJSViewer: {
+            type: 'grapesJSViewer',
+            model: []
         },
-        {
-          'inputName': 'stillImageFileValueHasDimX',
+        synopsisViewer: {
+            type: 'synopsisViewer',
+            model: []
         },
-        {
-          'inputName': 'stillImageFileValueHasDimY',
+        createResourceForm: {
+            type: 'createResourceForm',
+            model: []
         },
-        {
-          'inputName': 'stillImageFileValueHasIIIFBaseUrl',
+        editResourceForm: {
+            type: 'createResourceForm',
+            model: []
+        },
+        dataChooser: {
+            type: 'dataChooser',
+            model: []
+        },
+        textlistViewers: {
+            type: 'textlistViewers',
+            model: [],
+            inputs: [
+                {
+                    'inputName': 'textlist'
+                }
+            ]
+        },
+        barCharts: {
+            type: 'barCharts',
+            model: []
+        },
+        spiralBarCharts: {
+            type: 'spiralBarCharts',
+            model: []
+        },
+        lineCharts: {
+            type: 'lineCharts',
+            model: []
+        },
+        brushZoomCharts: {
+            type: 'brushZoomCharts',
+            model: []
+        },
+        leafletMaps: {
+            type: 'leafletMaps',
+            model: []
+        },
+        pieCharts: {
+            type: 'pieCharts',
+            model: []
+        },
+        radialBarCharts: {
+            type: 'radialBarCharts',
+            model: []
+        },
+        sankeyCharts: {
+            type: 'sankeyCharts',
+            model: []
+        },
+        stackedBarCharts: {
+            type: 'stackedBarCharts',
+            model: []
+        },
+        chordDiagrams: {
+            type: 'chordDiagrams',
+            model: []
+        },
+        simpleImageApp: {
+            type: 'simpleImageApp',
+            model: [],
+            inputs: [
+                {
+                    'inputName': 'imageURL'
+                }
+            ]
+        },
+        jsonViewer: {
+            type: 'jsonViewer',
+            model: [],
+            inputs: [
+                {
+                    'inputName': 'json'
+                }
+            ]
+        },
+        dataListView: {
+            type: 'dataListView',
+            model: [],
+            inputs: [
+                {
+                    'inputName': 'json'
+                }
+            ]
+        },
+        parzivalFassung: {
+            type: 'parzivalFassung',
+            model: [],
+            inputs: [
+                {
+                    'inputName': 'textJson'
+                }
+            ]
+        },
+        avpEditionView: {
+            type: 'avpEditionView',
+            model: [],
+            inputs: [
+                {
+                    'inputName': 'backendAddress'
+                }
+            ]
+        },
+        svgTranscription: {
+            type: 'svgTranscription',
+            model: [],
+            inputs: [
+                {
+                    'inputName': 'pageTree'
+                },
+                {
+                    'inputName': 'imageUrl'
+                },
+                {
+                    'inputName': 'imageWidth'
+                },
+                {
+                    'inputName': 'imageHeight'
+                }
+            ]
         }
-      ]
-    },
-    textViewer: {
-      type: 'textViewer',
-      model: []
-    },
-    projectViewer: {
-      type: 'projectViewer',
-      model: []
-    },
-    searchViewer: {
-      type: 'searchViewer',
-      model: []
-    },
-    grapesJSViewer: {
-      type: 'grapesJSViewer',
-      model: []
-    },
-    synopsisViewer: {
-      type: 'synopsisViewer',
-      model: []
-    },
-    createResourceForm: {
-      type: 'createResourceForm',
-      model: []
-    },
-    editResourceForm: {
-      type: 'createResourceForm',
-      model: []
-    },
-    dataChooser: {
-      type: 'dataChooser',
-      model: []
-    },
-    textlistViewers: {
-      type: 'textlistViewers',
-      model: [],
-      inputs: [
-        {
-          'inputName': 'textlist'
-        }
-      ]
-    },
-    barCharts: {
-      type: 'barCharts',
-      model: []
-    },
-    spiralBarCharts: {
-      type: 'spiralBarCharts',
-      model: []
-    },
-    lineCharts: {
-      type: 'lineCharts',
-      model: []
-    },
-    brushZoomCharts: {
-      type: 'brushZoomCharts',
-      model: []
-    },
-    leafletMaps: {
-      type: 'leafletMaps',
-      model: []
-    },
-    pieCharts: {
-      type: 'pieCharts',
-      model: []
-    },
-    radialBarCharts: {
-      type: 'radialBarCharts',
-      model: []
-    },
-    sankeyCharts: {
-      type: 'sankeyCharts',
-      model: []
-    },
-    stackedBarCharts: {
-      type: 'stackedBarCharts',
-      model: []
-    },
-    chordDiagrams: {
-      type: 'chordDiagrams',
-      model: []
-    },
-    simpleImageApp: {
-      type: 'simpleImageApp',
-      model: [],
-      inputs: [
-        {
-          'inputName': 'imageURL'
-        }
-      ]
-    },
-    jsonViewer: {
-      type: 'jsonViewer',
-      model: [],
-      inputs: [
-        {
-          'inputName': 'json'
-        }
-      ]
-    },
-    dataListView: {
-      type: 'dataListView',
-      model: [],
-      inputs: [
-        {
-          'inputName': 'json'
-        }
-      ]
-    },
-    parzivalFassung: {
-      type: 'parzivalFassung',
-      model: [],
-      inputs: [
-        {
-          'inputName': 'textJson'
-        }
-      ]
-    },
-    avpEditionView: {
-      type: 'avpEditionView',
-      model: [],
-      inputs: [
-        {
-          'inputName': 'backendAddress'
-        }
-      ]
-    },
-    svgTranscription: {
-      type: 'svgTranscription',
-      model: [],
-      inputs: [
-        {
-          'inputName': 'pageTree'
-        },
-        {
-          'inputName': 'imageUrl'
-        },
-        {
-          'inputName': 'imageWidth'
-        },
-        {
-          'inputName': 'imageHeight'
-        }
-      ]
-    }
-  };
+    };
 }
