dbca514eaef0717a9de8a4cadf01c30338b6dd23
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2019-07-22 15:44:13 +0200

    migrated apps to new page menu

--- src/app/app-engine/page/page/page.component.ts
+++ src/app/app-engine/page/page/page.component.ts
@@ -3,7 +3,7 @@
  * Data for the apps are loaded with the help of the load-variables component.
  * */
 
-import {AfterViewChecked, ChangeDetectorRef, Component, NgModule, OnInit, VERSION} from '@angular/core';
+import {AfterViewChecked, ChangeDetectorRef, Component, NgModule, OnInit, VERSION, ViewChild} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import 'rxjs/add/operator/map';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -20,6 +20,26 @@ import {PageMenuComponent} from '../page-menu/page-menu.component';
 import {Subscription} from 'rxjs';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import {ActionService} from '../../../user-action-engine/mongodb/action/action.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  color: string;
+}
+
+/** Constants used to fill up our data base. */
+const COLORS: string[] = [
+  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
+  'aqua', 'blue', 'navy', 'black', 'gray'
+];
+const NAMES: string[] = [
+  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
+  'Isabella', 'Jasperrrrrrrrrrrrrrr', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+];
 
 /**
  * @params actionID - the actionID of the action that the page belongs to
@@ -45,6 +65,11 @@ import {ActionService} from '../../../user-action-engine/mongodb/action/action.s
   providers: [StyleMappingService]
 })
 export class PageComponent implements OnInit, AfterViewChecked {
+  displayedColumns: string[] = ['id', 'name', 'tags', 'status'];
+  dataSource: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   actionID: string;
   page: any = {};
   action: any;
@@ -97,13 +122,233 @@ export class PageComponent implements OnInit, AfterViewChecked {
     public sanitizer: DomSanitizer,
     private stylemapping: StyleMappingService,
     private actionService: ActionService,
-    private router: Router
+    private router: Router,
   ) {
     this.route.queryParams.subscribe(params => {
       this.hashOfThisPage = params.page;
       this.actionID = params.actionID;
       this.generateNavigation(params.actionID);
     });
+    this.dataSource = new MatTableDataSource(
+      [
+        {
+          id: 'aspect_ratio',
+          name: 'HTML Viewer ',
+          tags: 'html, css, plaintext ',
+          color: 'green',
+          status: 'stable',
+          appType: 'htmlViewer',
+          description: 'It displays HTML snippets and applies externally defined styles to the view.'
+        },
+        {
+          id: 'short_text',
+          name: 'Plaintext Viewer ',
+          tags: 'plaintext, html without css ',
+          color: 'green',
+          status: 'stable',
+          appType: 'textlistViewers',
+          description: undefined
+        },
+        {
+          id: 'line_style',
+          name: 'SVG-Transcription',
+          tags: 'diplomatic transcription, text',
+          color: 'orange',
+          status: 'under development',
+          appType: 'svgTranscription',
+          description: ' SVG transcription with exact word positions'
+        },
+        {
+          id: 'view_week',
+          name: 'Parzival - Fassung',
+          tags: 'synopsis, text, version',
+          color: 'green',
+          status: 'stable',
+          appType: 'parzivalFassung',
+          description: 'Project specific App for the Parzival Project - Open more than one app of this type to test it!'
+        },
+        {
+          id: 'turned_in',
+          name: 'AVP-Edition',
+          tags: 'synopsis, text, version',
+          color: 'orange',
+          status: 'under development',
+          appType: 'avpEditionView',
+          description: 'Project specific App for the AVP Project'
+        },
+        {
+          id: 'album',
+          name: 'OpenBis Login',
+          tags: 'login, query, third party token',
+          color: 'green',
+          status: 'stable',
+          appType: 'openbisLogin',
+          description: 'Set token for OpenBis'
+        },
+        {
+          id: 'image',
+          name: 'Image Viewer',
+          tags: 'image, zoom, iif',
+          color: 'green',
+          status: 'stable',
+          appType: 'imageViewer',
+          description: 'Display and zoom in images'
+        },
+        {
+          id: 'add_photo_alternate',
+          name: 'Simple Image App',
+          tags: 'image',
+          color: 'green',
+          status: 'stable',
+          appType: 'simpleImageApp',
+          description: 'Display simple images'
+        },
+        {
+          id: 'view_list',
+          name: 'Data list viewer',
+          tags: 'generic, list, configurable',
+          color: 'green',
+          status: 'stable',
+          appType: 'dataListView',
+          description: 'Display simple images'
+        },
+        {
+          id: 'subdirectory_arrow_right',
+          name: 'Tree navigation',
+          tags: 'navigation',
+          color: 'green',
+          status: 'stable',
+          appType: 'treeNavigation',
+          description: ' This app can be combined with apps for resource view.'
+        },
+        {
+          id: 'video_library',
+          name: 'Youtube Video Viewer',
+          tags: 'video',
+          color: 'green',
+          status: 'stable',
+          appType: 'youtubeVideo',
+          description: ' You can embed youtube - videos in this app.'
+        },
+        {
+          id: 'art_track',
+          name: 'Synopsis Viewer',
+          tags: 'synopsis, light table',
+          color: 'orange',
+          status: 'under development',
+          appType: 'synopsisViewer',
+          description: 'Open and compare different images and text'
+        },
+        {
+          id: 'spa',
+          name: 'Json Viewer',
+          tags: 'json tree',
+          color: 'green',
+          status: 'stable',
+          appType: 'jsonViewer',
+          description: 'This app visualizes json objects as a tree.'
+        },
+        {
+          id: 'bar_chart',
+          name: 'Bar Chart',
+          tags: 'd3.js, interactive',
+          color: 'orange',
+          status: 'under development',
+          appType: 'barCharts',
+          description: 'To define data with two dimensions'
+        },
+        {
+          id: 'show_chart',
+          name: 'Line Chart',
+          tags: 'd3.js, interactive',
+          color: 'orange',
+          status: 'under development',
+          appType: 'lineCharts',
+          description: 'To define data with two dimensions'
+        },
+        {
+          id: 'insert_chart_outlined',
+          name: 'Brush Zoom',
+          tags: 'd3.js, interactive',
+          color: 'orange',
+          status: 'under development',
+          appType: 'brushZoomCharts',
+          description: 'To define data with two dimensions'
+        },
+        {
+          id: 'add_location',
+          name: 'Leaflet map',
+          tags: 'geo data visualisation',
+          color: 'orange',
+          status: 'under development',
+          appType: 'leafletMaps',
+          description: 'Visualize data on maps'
+        },
+        {
+          id: 'pie_chart',
+          name: 'Pie Chart',
+          tags: 'd3.js, data visualisation',
+          color: 'orange',
+          status: 'under development',
+          appType: 'pieCharts',
+          description: 'To visualise subsets'
+        },
+        {
+          id: 'multiline_chart',
+          name: 'Radial Bar Chart',
+          tags: 'd3.js, data visualisation',
+          color: 'orange',
+          status: 'under development',
+          appType: 'radialBarCharts',
+          description: 'For two dimensional data'
+        },
+        {
+          id: 'linear_scale',
+          name: 'Sankey',
+          tags: 'd3.js, data visualisation',
+          color: 'orange',
+          status: 'under development',
+          appType: 'sankeyCharts',
+          description: ' To display dependent data'
+        },
+        {
+          id: 'subtitles',
+          name: ' Stacked Bar Chart',
+          tags: 'd3.js, data visualisation',
+          color: 'orange',
+          status: 'under development',
+          appType: 'stackedBarCharts',
+          description: 'To display grouped sets'
+        },
+        {
+          id: 'all_out',
+          name: 'Chord diagram',
+          tags: 'd3.js, data visualisation',
+          color: 'orange',
+          status: 'under development',
+          appType: 'chordDiagrams',
+          description: 'To compare intensity of connections'
+        },
+        {
+          id: 'add_to_queue',
+          name: 'Resource Creation',
+          tags: 'knora, data entry',
+          color: 'orange',
+          status: 'under development',
+          appType: 'createResourceForm',
+          description: 'With this app you can create data in Knora.'
+        },
+        {
+          id: 'web',
+          name: 'Edit Resource',
+          tags: 'knora, data manipulation',
+          color: 'orange',
+          status: 'under development',
+          appType: 'editResourceForm',
+          description: 'With this app you can change data in Knora and see the history of given properties.'
+        }
+      ]
+    );
   }
 
   changeAppFramePosition() {
@@ -171,7 +416,17 @@ export class PageComponent implements OnInit, AfterViewChecked {
     this.cdr.detectChanges();
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
     console.log( this.route.snapshot );
     if (
       this.route.snapshot.url[0].path === 'home' &&
@@ -311,6 +566,9 @@ export class PageComponent implements OnInit, AfterViewChecked {
       appModel[ length ].title = appType + ' ' + length;
       appModel[ length ].fullWidth = false;
       appModel[ length ].fullHeight = false;
+      appModel[ length ].initialized = true;
+      appModel[ length ].x = 100;
+      appModel[ length ].y = 100;
       if (
         this.page.openApps &&
         this.page.openApps[ appModel[ length ].hash ] === null
@@ -450,4 +708,17 @@ export class PageComponent implements OnInit, AfterViewChecked {
     this.openAppsInThisPage[ 'pageMenu' ].model[ 0 ].y = 100;
   }
 
+}
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+  };
 }
