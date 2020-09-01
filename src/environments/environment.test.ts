70cfb55e3c099467bdc0c13a04bd9bc36dba120a
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2020-09-01 13:22:52 +0200

    microservice node and others

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -9,5 +9,5 @@ export const environment = {
   salsah2: 'http://salsah2.nie-ine.ch/',
   raeber: 'http://raeber.nie-ine.ch/',
   webern: 'https://edition.anton-webern.ch/',
-  jsonEnvironment: 'jsonEnvironment://jsonEnvironment:port'
+  jsonEnvironment: 'http://jsonEnvironment:50000'
 };
