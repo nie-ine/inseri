ed3f2495b170442562b8895a419a84523f8de9ab
domsteinbach <dominique.steinbach@unibas.ch>
2020-09-08 14:10:25 +0200

    Implemented detection and rendering of links inside text (in text/string only - not in proper html coming from inseri data ).

--- src/app/app-engine/apps/textlist-viewer/textlist-viewer.component.ts
+++ src/app/app-engine/apps/textlist-viewer/textlist-viewer.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input, OnChanges} from '@angular/core';
+import {Component, OnInit, Input, OnChanges } from '@angular/core';
 import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 import {HttpClient} from '@angular/common/http';
 
@@ -18,7 +18,6 @@ export class TextlistViewerComponent implements OnChanges {
   ) {
   }
   ngOnChanges() {
-    // console.log( 'Changes', this.textToDisplay );
     if (  this.textToDisplay && this.textToDisplay.search( 'http' ) !== -1 ) {
       this.http.get( this.textToDisplay, { responseType: 'text' } )
         .subscribe(
@@ -30,13 +29,33 @@ export class TextlistViewerComponent implements OnChanges {
               this.safeHtml = this.domSanitizer.bypassSecurityTrustHtml( data );
             }
           }, error => {
-            this.safeHtml = this.domSanitizer.bypassSecurityTrustHtml(this.textToDisplay);
+            this.safeHtml = this.domSanitizer.bypassSecurityTrustHtml(this.linkify(this.textToDisplay));
             // console.log( error );
           }
         );
     } else {
-      this.safeHtml = this.domSanitizer.bypassSecurityTrustHtml(this.textToDisplay);
+      this.safeHtml = this.domSanitizer.bypassSecurityTrustHtml(this.linkify(this.textToDisplay));
     }
+  }
 
+  linkify(linkText: string) {
+    let replacedText;
+    let replacePattern1;
+    let replacePattern2;
+    let replacePattern3;
+
+    // URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = linkText.toString().replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    // URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    // Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
   }
 }
