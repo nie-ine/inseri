eb747a502f63b8cfc6741bde10e56a05babff99b
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2020-09-23 15:33:52 +0200

    #552 done

--- src/app/app-engine/apps/crispr/crispr.component.ts
+++ src/app/app-engine/apps/crispr/crispr.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {Validators} from '@angular/forms';
 import {HttpClient} from '@angular/common/http';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-crispr',
@@ -11,8 +13,11 @@ export class CrisprComponent {
   fileToUpload: any;
   form = new FormData();
   showSubmitButton = false;
+  pathToFile: SafeResourceUrl;
+  fileHasChanged = false;
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private sanitizer: DomSanitizer
   ) {}
 
   onFileChange(files: FileList): void {
@@ -20,15 +25,20 @@ export class CrisprComponent {
     if ( files.item(0) ) {
       this.fileToUpload = files.item(0);
       this.showSubmitButton = true;
+      this.fileHasChanged = true;
     }
 
     this.form.append('data', this.fileToUpload, 'data' );
   }
 
   submitFile() {
-    this.http.post('http://localhost:4321', this.form)
+    this.http.post('http://localhost:4321', this.form, { responseType: 'blob' })
       .subscribe((val) => {
-        console.log(val);
+        const blob = new Blob([ val as any ], { type: 'application/pdf' });
+        console.log( blob );
+        const url = URL.createObjectURL(blob);
+        this.pathToFile = this.sanitizer.bypassSecurityTrustResourceUrl( url );
+        this.fileHasChanged = false;
     }, error => console.log( error ));
   }
 }
