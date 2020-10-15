ce3290a0a0ba415dce0cd38d47c99c8bd1654c3a
rfbaumgartner <rfbaumgartner@users.noreply.github.com>
2020-10-15 15:51:40 +0200

    remove unfinished component for linear glossing

--- src/app/app-engine/apps/complex-text-views/complex-text-views.module.ts
+++ src/app/app-engine/apps/complex-text-views/complex-text-views.module.ts
@@ -4,7 +4,6 @@ import { TextPageOfWordsComponent } from './text-page-of-words/text-page-of-word
 import { TextStructureModule } from '../text-structure/text-structure.module';
 import { RichTextModule } from '../shared/rich-text/rich-text.module';
 import { TextSvgViewComponent } from './text-svg-view/text-svg-view.component';
-import { InterlinearGlossingComponent } from './interlinear-glossing/interlinear-glossing.component';
 import { TextSvgViewWrapperComponent } from './text-svg-view-app-wrapper/text-svg-view-wrapper.component';
 
 @NgModule({
@@ -13,7 +12,7 @@ import { TextSvgViewWrapperComponent } from './text-svg-view-app-wrapper/text-sv
     RichTextModule,
     TextStructureModule
   ],
-  declarations: [InterlinearGlossingComponent, TextPageOfWordsComponent, TextSvgViewComponent, TextSvgViewWrapperComponent],
-  exports: [InterlinearGlossingComponent, TextPageOfWordsComponent, TextSvgViewComponent, TextSvgViewWrapperComponent]
+  declarations: [TextPageOfWordsComponent, TextSvgViewComponent, TextSvgViewWrapperComponent],
+  exports: [TextPageOfWordsComponent, TextSvgViewComponent, TextSvgViewWrapperComponent]
 })
 export class ComplexTextViewsModule { }
