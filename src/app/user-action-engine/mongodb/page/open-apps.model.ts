9ff4753cfc14935de4fc2274ca19ef5530b03176
Cyrill Martin <cyrill.martin@unibas.ch>
2020-12-04 12:03:07 +0100

    Add comments about still mentioned jsonEnvironment

--- src/app/user-action-engine/mongodb/page/open-apps.model.ts
+++ src/app/user-action-engine/mongodb/page/open-apps.model.ts
@@ -198,7 +198,7 @@ export class OpenAppsModel {
       initialWidth: '400',
       initialHeight: '400'
     },
-    jsonEnvironment: {
+    jsonEnvironment: { // This is the old name of the now called pythonEnvironment
       type: 'jsonEnvironment',
       model: [],
       inputs: [
