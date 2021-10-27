02524c1929396a7c77f0da588b34cfaed49e2ff1
janCstoffregen <jan.stoffregen@zi.uzh.ch>
2021-10-27 13:50:37 +0200

    update to angular 12

---
+++ src/environments/environment.unibas.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production: true,
+    name: 'nieOS',
+    api: 'http://localhost:3333',
+    app: 'https://unibas.inseri.swiss',
+    media: 'http://localhost:1024',
+    node: 'https://node-unibas.inseri.swiss',
+    sparqlVisualizer: 'http://sparqlvisualizer.nie-ine.ch/',
+    salsah2: 'http://salsah2.nie-ine.ch/',
+    raeber: 'http://raeber.nie-ine.ch/',
+    webern: 'https://edition.anton-webern.ch/',
+    pythonEnvironment: 'http://jsonenvironment:50000',
+    machineReasoning: 'http://machineReasoning:50001',
+    crispr: 'http://172.23.39.73:4321'
+  };
+  
