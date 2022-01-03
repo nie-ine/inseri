99de4297caf9c72e0475d1416111436917c528fc
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-01-03 10:47:48 +0100

    use deployed values for production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,14 @@ export const environment = {
   production: true,
   name: 'nieOS',
   api: 'http://localhost:3333',
-  app: 'https://nieos.nie-ine.ch',
+  app: 'https://inseri.swiss',
   media: 'http://localhost:1024',
-  node: 'https://node.nie-ine.ch',
+  node: 'https://node.inseri.swiss',
   sparqlVisualizer: 'http://sparqlvisualizer.nie-ine.ch/',
   salsah2: 'http://salsah2.nie-ine.ch/',
   raeber: 'http://raeber.nie-ine.ch/',
   webern: 'https://edition.anton-webern.ch/',
+  pythonEnvironment: 'http://jsonenvironment:50000',
+  machineReasoning: 'http://machineReasoning:50001',
   crispr: 'http://172.23.39.73:4321'
 };
