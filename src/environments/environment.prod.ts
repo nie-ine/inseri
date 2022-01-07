6da7996809d40f94c26c80ba5ac8615febc46f59
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-01-07 18:17:35 +0100

    point to correct sparql visualizer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   app: 'https://inseri.swiss',
   media: 'http://localhost:1024',
   node: 'https://node.inseri.swiss',
-  sparqlVisualizer: 'http://sparqlvisualizer.nie-ine.ch/',
+  sparqlVisualizer: 'https://sparqlvisualiser.inseri.swiss/',
   salsah2: 'http://salsah2.nie-ine.ch/',
   raeber: 'http://raeber.nie-ine.ch/',
   webern: 'https://edition.anton-webern.ch/',
