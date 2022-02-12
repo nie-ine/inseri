2303157d1fe666f679def3e13f05c5426e3fb890
Arooran Thanabalasingam <arooran.thanabalasingam@uzh.ch>
2022-02-13 00:52:38 +0100

    test GET /users

--- backend/test/user.spec.ts
+++ backend/test/user.spec.ts
@@ -6,6 +6,34 @@ import 'mocha';
 chai.use(chaiHttp);
 chai.should()
 
+// TODO verify if endpoint is still needed
+describe('GET /users', () => {
+  describe('given one user', () => {
+    it("returns 200", async() => {
+      const {status} = await chai.request(server).get('/api/users')
+      status.should.equal(200)
+    })
+
+    it("returns message", async() => {
+      const {body} = await chai.request(server).get('/api/users')
+      body.message.should.equal('One pages was found')
+    })
+
+    it("returns user object", async() => {
+      const {body} = await chai.request(server).get('/api/users')
+      body.user.should.deep.equal([{
+        __v:0,
+        _id: '620522b4fc13ae03b300031b',
+        email: 'foo.bar@inseri.swiss',
+        firstName: 'Foo',
+        lastName: 'Bar',
+        newsletter: false,
+        usrProfileFilePath: '/home/pic/foo.jpg',
+        password: "mysecret1234"
+      }])
+    })
+  })
+})
 
 describe('GET /users/:id', () => {
   describe('given non-existing user', () => {
@@ -20,6 +48,8 @@ describe('GET /users/:id', () => {
     })
   })
 
+  // TODO hide error object
+  // TODO return 400
   describe('given nonsense as id', () => {
     it("returns 500", async() => {
       const {status} = await chai.request(server).get('/api/users/nonsense')
